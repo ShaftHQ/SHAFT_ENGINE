e0cbe2dcc2d032425788188e52e3d3aea1f6a34a
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2018-10-02 15:22:40 +0200

    Fixes to assertions, verifications, and element identification delay...
    . Assertions: formatting changes, added
    retriesBeforeThrowingElementNotFoundException to improve performance for
    assertElementExists, added comparisonType to assertElementAttribute and
    assertBrowserAttribute.
    . BrowserActions: formatting changes, removed validation for currentURL
    after navigating to a new URL and added commented justification.
    . ElementActions: formatting changes, honored
    retriesBeforeThrowingElementNotFoundException dynamically for
    getElementsCount, countFoundElements, and internalCanFindUniqueElement
    . Verifications: formatting changes, added
    retriesBeforeThrowingElementNotFoundException to improve performance for
    verifyElementExists, added comparisonType to verifyElementAttribute and
    verifyBrowserAttribute.
    .tests_validations_assertions: added many new tests

--- src/main/java/com/shaft/io/ReportManager.java
+++ src/main/java/com/shaft/io/ReportManager.java
@@ -16,260 +16,248 @@ import com.shaft.support.SSHActions;
 
 public class ReportManager {
 
-	private static String fullLog = "";
-	private static int actionCounter = 1;
-	private static boolean isHeaderTyped = false;
-	private static boolean log = true;
-	private static String lastTestCase = "";
+    private static String fullLog = "";
+    private static int actionCounter = 1;
+    private static boolean isHeaderTyped = false;
+    private static boolean log = true;
+    private static String lastTestCase = "";
 
-	private ReportManager() {
-		throw new IllegalStateException("Utility class");
+    private ReportManager() {
+	throw new IllegalStateException("Utility class");
+    }
+
+    /**
+     * Manages action counter and calls writeLog to format and print the log entry.
+     * 
+     * @param logText
+     *            the text that needs to be logged in this action
+     */
+    public static void log(String logText) {
+	logEngineVersionAndEnvironmentData();
+	logTestInformation();
+
+	if (log) {
+	    writeLogStepToReport(logText, actionCounter);
+	    actionCounter++;
 	}
+    }
 
-	/**
-	 * Manages action counter and calls writeLog to format and print the log entry.
-	 * 
-	 * @param logText
-	 *            the text that needs to be logged in this action
+    /**
+     *
+     * Format an exception message and stack trace, and calls attach to add it as a
+     * log entry.
+     * 
+     * @param e
+     *            the exception that will be logged in this action
+     */
+    public static void log(Exception e) {
+	logEngineVersionAndEnvironmentData();
+	logTestInformation();
+
+	if (log) {
+	    StringBuilder logBuilder = new StringBuilder();
+	    String logText = "";
+	    StackTraceElement[] trace = e.getStackTrace();
+
+	    // enhance to include exception type
+
+	    logBuilder.append(e.getClass().getName() + ":" + System.lineSeparator() + e.getMessage() + System.lineSeparator());
+
+	    for (int i = 0; i < trace.length; ++i) {
+		logBuilder.append(trace[i].toString() + System.lineSeparator());
+	    }
+	    logText = logBuilder.toString();
+
+	    attach("Exception Stack Trace for [" + e.getClass().getName() + "]", logText);
+	    actionCounter++;
+	}
+    }
+
+    /**
+     * Formats logText and adds timestamp, then logs it as a step in the execution
+     * report.
+     * 
+     * @param logText
+     *            the text that needs to be logged in this action
+     * @param actionCounter
+     *            a number that represents the serial number of this action within
+     *            this test run
+     */
+    @Step("Action [{actionCounter}]: {logText}")
+    private static void writeLogStepToReport(String logText, int actionCounter) {
+	performLogEntry(logText);
+    }
+
+    private static void performLogEntry(String logText) {
+	String timestamp = (new SimpleDateFormat("dd-MM-yyyy HH:mm:ss.SSSS aaa")).format(new Date(System.currentTimeMillis()));
+
+	String log = "[ReportManager] " + logText.trim() + " @" + timestamp + System.lineSeparator();
+	Reporter.log(log, true);
+	appendToFullLog(log);
+    }
+
+    /**
+     * Adds a new attachment using the input parameters provided. The attachment is
+     * displayed as a step in the execution report. Used for Screenshots.
+     * 
+     * @param attachmentType
+     *            the type of this attachment
+     * @param attachmentName
+     *            the name of this attachment
+     * @param attachmentContent
+     *            the content of this attachment
+     */
+    @Step("Attachement: {attachmentType} - {attachmentName}")
+    public static void attach(String attachmentType, String attachmentName, InputStream attachmentContent) {
+	if (log) {
+	    Allure.addAttachment(attachmentName, attachmentContent);
+	    performLogEntry("Successfully created attachment [" + attachmentType + " - " + attachmentName + "]");
+	}
+    }
+
+    /**
+     * Adds a new attachment using the input parameters provided. The attachment is
+     * displayed as a step in the execution report. Used for REST API Responses.
+     * 
+     * @param attachmentName
+     *            the name of this attachment
+     * @param attachmentContent
+     *            the content of this attachment
+     */
+    @Step("Attachment: {attachmentName}")
+    public static void attach(String attachmentName, String attachmentContent) {
+	if (log) {
+	    Allure.addAttachment(attachmentName, attachmentContent);
+	    performLogEntry("Successfully created attachment [" + attachmentName + "]");
+	}
+    }
+
+    /**
+     * Returns the log of the current test, and attaches it in the end of the test
+     * execution report.
+     * 
+     * @return the log for the current test
+     */
+    @Attachment("Test log")
+    public static String getTestLog() {
+
+	StringBuilder logBuilder = new StringBuilder();
+	String logText = "";
+
+	for (String s : Reporter.getOutput()) {
+	    s = s.replace("<br>", System.lineSeparator());
+	    logBuilder.append(s);
+	}
+	logText = logBuilder.toString();
+	Reporter.clear();
+	return logText;
+
+	// String log = "";
+	//
+	// for (String s : Reporter.getOutput()) {
+	// s = s.replace("<br>", System.lineSeparator());
+	// log = log + s;
+	// }
+	// Reporter.clear();
+	// return log;
+    }
+
+    /**
+     * Returns the complete log of the current execution session, and attaches it in
+     * the end of the test execution report.
+     * 
+     */
+    public static void getFullLog() {
+	attachFullLog();
+	if (Boolean.valueOf(System.getProperty("automaticallyGenerateAllureReport").trim())) {
+	    generateAllureReportArchive();
+	}
+    }
+
+    @Attachment("Full log")
+    private static String attachFullLog() {
+	return fullLog;
+    }
+
+    /**
+     * Appends a log entry to the complete log of the current execution session.
+     * 
+     * @param log
+     *            the log entry that needs to be appended to the full log
+     */
+    private static void appendToFullLog(String log) {
+	fullLog += log;
+    }
+
+    private static void generateAllureReportArchive() {
+	log("Generating Allure Report Archive...");
+	log = false;
+	List<String> commandToCreateAllureReport = Arrays.asList("src/main/resources/allure/bin/allure generate \"allure-results\" -o \"generatedReport/allure-report\"");
+
+	(new SSHActions()).executeShellCommand(commandToCreateAllureReport);
+
+	FileManager.copyFolder(FileManager.getAbsolutePath("src/main/resources/", "allure"), "generatedReport/allure");
+
+	// "src/main/resources/allure/bin/allure open \"allure-report\""
+
+	List<String> commandToOpenAllureReport = Arrays.asList("#!/bin/bash", "parent_path=$( cd \"$(dirname \"${BASH_SOURCE[0]}\")\" ; pwd -P )", "cd \"$parent_path/allure/bin/\"", "bash allure open \"$parent_path/allure-report\"", "exit");
+	FileManager.writeToFile("generatedReport/", "open_allure_report.sh", commandToOpenAllureReport);
+
+	FileManager.zipFiles("generatedReport/", "generatedReport.zip");
+
+	FileManager.deleteFile("generatedReport/");
+	log = true;
+	log("This test run was powered by SHAFT Engine Version: [" + System.getProperty("shaftEngineVersion") + "] © Copyrights Reserved to [Mohab Mohie].");
+    }
+
+    private static void populateEnvironmentData() {
+	// sets up some parameters to the allure report
+	FileManager.writeToFile(System.getProperty("allureResultsFolderPath"), "environment.properties", Arrays.asList("Engine=" + System.getProperty("shaftEngineVersion"), "OS=" + System.getProperty("targetOperatingSystem"), "Browser=" + System.getProperty("targetBrowserName"), "Location=" + System.getProperty("executionAddress")));
+	/*
+	 * "Flags_AutoMaximizeBrowserWindow=" +
+	 * System.getProperty("autoMaximizeBrowserWindow"), "Flags_ScreenshotManager=" +
+	 * System.getProperty("screenshotParams_whenToTakeAScreenshot"),
+	 * "Config_DefaultElementIdentificationTimeout=" +
+	 * System.getProperty("defaultElementIdentificationTimeout")));
 	 */
-	public static void log(String logText) {
-		logEngineVersionAndEnvironmentData();
-		logTestInformation();
+    }
 
-		if (log) {
-			writeLogStepToReport(logText, actionCounter);
-			actionCounter++;
-		}
+    private static void logEngineVersion() {
+	log("Detected SHAFT Engine Version: [" + System.getProperty("shaftEngineVersion") + "] © Copyrights Reserved to [Mohab Mohie].");
+	// calls parent log function recursively in order to log shaft version before
+	// performing the first action
+    }
+
+    private static void logEngineVersionAndEnvironmentData() {
+	if (!isHeaderTyped) {
+	    isHeaderTyped = true;
+
+	    logEngineVersion();
+	    populateEnvironmentData();
+	}
+    }
+
+    private static void logTestInformation() {
+	// try {
+	String currentTestCase = "";
+
+	if (Reporter.getCurrentTestResult().getMethod().getDescription() != null) {
+	    currentTestCase = Reporter.getCurrentTestResult().getMethod().getDescription();
+	} else {
+	    currentTestCase = Reporter.getCurrentTestResult().getMethod().getMethodName();
 	}
 
-	/**
-	 *
-	 * Format an exception message and stack trace, and calls attach to add it as a
-	 * log entry.
-	 * 
-	 * @param e
-	 *            the exception that will be logged in this action
-	 */
-	public static void log(Exception e) {
-		logEngineVersionAndEnvironmentData();
-		logTestInformation();
-
-		if (log) {
-			StringBuilder logBuilder = new StringBuilder();
-			String logText = "";
-			StackTraceElement[] trace = e.getStackTrace();
-
-			// enhance to include exception type
-
-			logBuilder.append(
-					e.getClass().getName() + ":" + System.lineSeparator() + e.getMessage() + System.lineSeparator());
-
-			for (int i = 0; i < trace.length; ++i) {
-				logBuilder.append(trace[i].toString() + System.lineSeparator());
-			}
-			logText = logBuilder.toString();
-
-			attach("Exception Stack Trace for [" + e.getClass().getName() + "...]", logText);
-			actionCounter++;
-		}
+	if ((!currentTestCase.equals(lastTestCase)) && (!currentTestCase.equals(""))) {
+	    String testClass = Reporter.getCurrentTestResult().getTestClass().getName();
+	    performLogEntry("Starting Execution; class name [" + testClass + "], and test name [" + currentTestCase + "].");
+	    lastTestCase = currentTestCase;
 	}
-
-	/**
-	 * Formats logText and adds timestamp, then logs it as a step in the execution
-	 * report.
-	 * 
-	 * @param logText
-	 *            the text that needs to be logged in this action
-	 * @param actionCounter
-	 *            a number that represents the serial number of this action within
-	 *            this test run
-	 */
-	@Step("Action [{actionCounter}]: {logText}")
-	private static void writeLogStepToReport(String logText, int actionCounter) {
-		performLogEntry(logText);
-	}
-
-	private static void performLogEntry(String logText) {
-		String timestamp = (new SimpleDateFormat("dd-MM-yyyy HH:mm:ss.SSSS aaa"))
-				.format(new Date(System.currentTimeMillis()));
-
-		String log = "[ReportManager] " + logText.trim() + " @" + timestamp + System.lineSeparator();
-		Reporter.log(log, true);
-		appendToFullLog(log);
-	}
-
-	/**
-	 * Adds a new attachment using the input parameters provided. The attachment is
-	 * displayed as a step in the execution report. Used for Screenshots.
-	 * 
-	 * @param attachmentType
-	 *            the type of this attachment
-	 * @param attachmentName
-	 *            the name of this attachment
-	 * @param attachmentContent
-	 *            the content of this attachment
-	 */
-	@Step("Attachement: {attachmentType} - {attachmentName}")
-	public static void attach(String attachmentType, String attachmentName, InputStream attachmentContent) {
-		if (log) {
-			Allure.addAttachment(attachmentName, attachmentContent);
-			performLogEntry("Successfully created attachment [" + attachmentType + " - " + attachmentName + "]");
-		}
-	}
-
-	/**
-	 * Adds a new attachment using the input parameters provided. The attachment is
-	 * displayed as a step in the execution report. Used for REST API Responses.
-	 * 
-	 * @param attachmentName
-	 *            the name of this attachment
-	 * @param attachmentContent
-	 *            the content of this attachment
-	 */
-	@Step("Attachment: {attachmentName}")
-	public static void attach(String attachmentName, String attachmentContent) {
-		if (log) {
-			Allure.addAttachment(attachmentName, attachmentContent);
-			performLogEntry("Successfully created attachment [" + attachmentName + "]");
-		}
-	}
-
-	/**
-	 * Returns the log of the current test, and attaches it in the end of the test
-	 * execution report.
-	 * 
-	 * @return the log for the current test
-	 */
-	@Attachment("Test log")
-	public static String getTestLog() {
-
-		StringBuilder logBuilder = new StringBuilder();
-		String logText = "";
-
-		for (String s : Reporter.getOutput()) {
-			s = s.replace("<br>", System.lineSeparator());
-			logBuilder.append(s);
-		}
-		logText = logBuilder.toString();
-		Reporter.clear();
-		return logText;
-
-		// String log = "";
-		//
-		// for (String s : Reporter.getOutput()) {
-		// s = s.replace("<br>", System.lineSeparator());
-		// log = log + s;
-		// }
-		// Reporter.clear();
-		// return log;
-	}
-
-	/**
-	 * Returns the complete log of the current execution session, and attaches it in
-	 * the end of the test execution report.
-	 * 
-	 */
-	public static void getFullLog() {
-		attachFullLog();
-		if (Boolean.valueOf(System.getProperty("automaticallyGenerateAllureReport").trim())) {
-			generateAllureReportArchive();
-		}
-	}
-
-	@Attachment("Full log")
-	private static String attachFullLog() {
-		return fullLog;
-	}
-
-	/**
-	 * Appends a log entry to the complete log of the current execution session.
-	 * 
-	 * @param log
-	 *            the log entry that needs to be appended to the full log
-	 */
-	private static void appendToFullLog(String log) {
-		fullLog += log;
-	}
-
-	private static void generateAllureReportArchive() {
-		log("Generating Allure Report Archive...");
-		log = false;
-		List<String> commandToCreateAllureReport = Arrays.asList(
-				"src/main/resources/allure/bin/allure generate \"allure-results\" -o \"generatedReport/allure-report\"");
-
-		(new SSHActions()).executeShellCommand(commandToCreateAllureReport);
-
-		FileManager.copyFolder(FileManager.getAbsolutePath("src/main/resources/", "allure"), "generatedReport/allure");
-
-		// "src/main/resources/allure/bin/allure open \"allure-report\""
-
-		List<String> commandToOpenAllureReport = Arrays.asList("#!/bin/bash",
-				"parent_path=$( cd \"$(dirname \"${BASH_SOURCE[0]}\")\" ; pwd -P )", "cd \"$parent_path/allure/bin/\"",
-				"bash allure open \"$parent_path/allure-report\"", "exit");
-		FileManager.writeToFile("generatedReport/", "open_allure_report.sh", commandToOpenAllureReport);
-
-		FileManager.zipFiles("generatedReport/", "generatedReport.zip");
-
-		FileManager.deleteFile("generatedReport/");
-		log = true;
-		log("This test run was powered by SHAFT Engine Version: [" + System.getProperty("shaftEngineVersion")
-				+ "] © Copyrights Reserved to [Mohab Mohie].");
-	}
-
-	private static void populateEnvironmentData() {
-		// sets up some parameters to the allure report
-		FileManager.writeToFile(System.getProperty("allureResultsFolderPath"), "environment.properties",
-				Arrays.asList("Engine=" + System.getProperty("shaftEngineVersion"),
-						"OS=" + System.getProperty("targetOperatingSystem"),
-						"Browser=" + System.getProperty("targetBrowserName"),
-						"Location=" + System.getProperty("executionAddress")));
-		/*
-		 * "Flags_AutoMaximizeBrowserWindow=" +
-		 * System.getProperty("autoMaximizeBrowserWindow"), "Flags_ScreenshotManager=" +
-		 * System.getProperty("screenshotParams_whenToTakeAScreenshot"),
-		 * "Config_DefaultElementIdentificationTimeout=" +
-		 * System.getProperty("defaultElementIdentificationTimeout")));
-		 */
-	}
-
-	private static void logEngineVersion() {
-		log("Detected SHAFT Engine Version: [" + System.getProperty("shaftEngineVersion")
-				+ "] © Copyrights Reserved to [Mohab Mohie].");
-		// calls parent log function recursively in order to log shaft version before
-		// performing the first action
-	}
-
-	private static void logEngineVersionAndEnvironmentData() {
-		if (!isHeaderTyped) {
-			isHeaderTyped = true;
-
-			logEngineVersion();
-			populateEnvironmentData();
-		}
-	}
-
-	private static void logTestInformation() {
-		// try {
-		String currentTestCase = "";
-
-		if (Reporter.getCurrentTestResult().getMethod().getDescription() != null) {
-			currentTestCase = Reporter.getCurrentTestResult().getMethod().getDescription();
-		} else {
-			currentTestCase = Reporter.getCurrentTestResult().getMethod().getMethodName();
-		}
-
-		if ((!currentTestCase.equals(lastTestCase)) && (!currentTestCase.equals(""))) {
-			String testClass = Reporter.getCurrentTestResult().getTestClass().getName();
-			performLogEntry(
-					"Starting Execution; class name [" + testClass + "], and test name [" + currentTestCase + "].");
-			lastTestCase = currentTestCase;
-		}
-		// } catch (Throwable e) {
-		// several errors are thrown in case of calling this method outside a test case,
-		// like in browser setup or tear-down, therefore the correct response here is to
-		// do nothing
-		// this method gets called with every single action
-		// }
-	}
+	// } catch (Throwable e) {
+	// several errors are thrown in case of calling this method outside a test case,
+	// like in browser setup or tear-down, therefore the correct response here is to
+	// do nothing
+	// this method gets called with every single action
+	// }
+    }
 
 }
