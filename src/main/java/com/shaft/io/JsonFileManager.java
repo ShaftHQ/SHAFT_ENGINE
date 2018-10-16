f4906beb21c2d0278d9c2380afc0f312f6f94537
mennamagedm <menna.maged@incorta.com>
2018-10-16 15:21:50 +0200

    Adding two methods to compare two JsonObjects:

--- src/main/java/com/shaft/io/JsonFileManager.java
+++ src/main/java/com/shaft/io/JsonFileManager.java
@@ -4,38 +4,84 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-import org.json.simple.JSONArray;
+import org.json.JSONException;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.skyscreamer.jsonassert.JSONCompare;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.skyscreamer.jsonassert.JSONCompareResult;
+import org.testng.Assert;
 
 public class JsonFileManager {
 
+	JSONObject expactedJsonObject;
 
 	/**
-	 * Creates a new instance of the json file reader using the expected json
-	 * file path
+	 * Creates a new instance of the json file reader using the expected json file
+	 * path
 	 * 
 	 * @param jsFilePath
 	 *            the expected path for the target json file
 	 */
 	public JsonFileManager(String jsFilePath) {
-		
+
 		JSONParser parser = new JSONParser();
-		
 		try {
-			 
-	            JSONObject jsonObject = (JSONObject) parser.parse(new FileReader(jsFilePath));
-	            JSONArray jsonArray = (JSONArray) jsonObject.get("$");
-	            System.out.println(jsonArray);
-	 
-	        } catch (FileNotFoundException e) {
-	            e.printStackTrace();
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        } catch (ParseException e) {
-	            e.printStackTrace();
-	        }
-	 
-	    }
+			expactedJsonObject = (JSONObject) parser.parse(new FileReader(jsFilePath));
+
+		} catch (FileNotFoundException e) {
+			ReportManager.log(e);
+		} catch (IOException e) {
+			ReportManager.log(e);
+		    ReportManager.log("Couldn't find the desired file. [" + jsFilePath + "].");
+		    Assert.fail("Couldn't find the desired file. [" + jsFilePath + "].");
+		} catch (ParseException e) {
+			 ReportManager.log(e);
+		}
+	}
+
+	/**
+	 * Receive actual jsonObject of response and compare it with expected one
+	 * initialized in the constructor then return comparison result as boolean value
+	 * (true if two objects are strictly equal, otherwise return false)
+	 * 
+	 * @param actualJsonObject
+	 *            JSONObject
+	 * @return boolean value
+	 */
+
+	public boolean compareEqual_Strict_Order(JSONObject actualJsonObject) {
+		JSONCompareResult result = null;
+		try {
+			result = JSONCompare.compareJSON(actualJsonObject.toJSONString(), expactedJsonObject.toJSONString(),
+					JSONCompareMode.STRICT);
+		} catch (JSONException e) {
+			 ReportManager.log(e);
+		}
+		return result.passed();
+	}
+
+	/**
+	 * Receive actual jsonObject of response and compare it with expected one
+	 * initialized in the constructor then return comparison result as boolean value
+	 * (true if two objects are equal, otherwise return false)
+	 * 
+	 * It is non-strict comparison (ignore the elements' order)
+	 * 
+	 * @param actualJsonObject
+	 *            JSONObject
+	 * @return boolean value
+	 */
+
+	public boolean compareEqual_NonStrict_Order(JSONObject actualJsonObject) {
+		JSONCompareResult result = null;
+		try {
+			result = JSONCompare.compareJSON(actualJsonObject.toJSONString(), expactedJsonObject.toJSONString(),
+					JSONCompareMode.NON_EXTENSIBLE);
+		} catch (JSONException e) {
+			 ReportManager.log(e);
+		}
+		return result.passed();
+	}
 }
