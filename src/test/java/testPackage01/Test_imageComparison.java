4d0eba27c9cb1c29432e16c9cf9be82246a969a0
Mohab Mohie <Mohab.Mohie@incorta.com>
2018-10-31 11:55:15 +0200

    Initial commit with video recording support.
    .RecordManager: adding initial support for video recording.
    .BrowserActions: fixed two issues with maximizeWindow, styling changes.
    .BrowserFactory: migrated from DesiredCapabilities to browser specific
    options, styling changes.
    .ImageProcessingActions: logical fixes and reporting enhancements,
    styling changes.
    pom.xml: version bump, added recordVideo, fixed warnings related to
    encoding.type, included monte-repack dependency.
    .ReportManager: added special handling based on different
    attachmentTypes, integrated support for start/stop recording via the
    RecordManager.

---
+++ src/test/java/testPackage01/Test_imageComparison.java
@@ -0,0 +1,15 @@
+package testPackage01;
+
+import org.testng.annotations.Test;
+
+import com.shaft.image.ImageProcessingActions;
+
+public class Test_imageComparison {
+    @Test
+    public void f() {
+	String refrenceFolderPath = "src/test/resources/TestDataFiles/imageComparer/reference";
+	String testFolderPath = "src/test/resources/TestDataFiles/imageComparer/test";
+
+	ImageProcessingActions.compareImageFolders(refrenceFolderPath, testFolderPath, 98);
+    }
+}
