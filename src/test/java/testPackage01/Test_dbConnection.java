1c6773472076ce22bc0443569af44b4a5dc6b7e0
mennamagedm <menna.maged@incorta.com>
2019-06-17 17:40:34 +0200

    Support Postgresql DB connection

--- src/test/java/testPackage01/Test_dbConnection.java
+++ src/test/java/testPackage01/Test_dbConnection.java
@@ -11,41 +11,54 @@ import com.shaft.support.DBActions;
 public class Test_dbConnection {
 	
 	DBActions dbActions;
-	ResultSet QR;
+	ResultSet queryResult;
 
-	@Test
+	//@Test
 	public void test_mySQLConnection() {
 		dbActions = new DBActions("mysql","72.55.136.25","3306","menna","incorta","incorta_123");
 		dbActions.createConnection();
-		QR= dbActions.executeSelectQuery("select * from DASHBOARD;");
+		queryResult= dbActions.executeSelectQuery("select * from DASHBOARD;");
 		
 		try {
-			while(QR.next())
+			while(queryResult.next())
 			{
-				System.out.print(QR.getString(2)); //or rs.getString("column name");
+				System.out.print(queryResult.getString(2)); //or rs.getString("column name");
 			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-	@Test
+	//@Test
     public void test_SQLServerConnection() {
 		dbActions = new DBActions("sqlserver","72.55.136.25","1432","salesdb","sa","1nc0rta_123");
 		dbActions.createConnection();
-		QR= dbActions.executeSelectQuery("select * from abdelsalam.dbo.automation_table");
+		queryResult= dbActions.executeSelectQuery("select * from abdelsalam.dbo.automation_table");
 		
 		try {
-			while(QR.next())
+			while(queryResult.next())
 			{
-				System.out.print(QR.getString(2)); //or rs.getString("column name");
+				System.out.print(queryResult.getString(2)); //or rs.getString("column name");
 			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+    }
+	
+	@Test
+    public void test_PostgreSQLConnection() {
+		dbActions = new DBActions("postgresql","72.55.136.25","5432","demo","dev","dev_incorta");
+		dbActions.createConnection();
+		queryResult= dbActions.executeSelectQuery("select * from account");
+		
+		try {
+			while(queryResult.next())
+			{
+				System.out.print(queryResult.getString(2)); //or rs.getString("column name");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
     }
 
 	@AfterClass // Tear-down method, to be run once after the last test
