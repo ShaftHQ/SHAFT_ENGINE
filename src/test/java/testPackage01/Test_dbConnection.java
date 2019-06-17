3b6b93230fa7f7609f1e2a21f95861a54d932743
mennamagedm <menna.maged@incorta.com>
2019-06-17 12:20:01 +0200

    support SQL Server DB connector to DBActions

--- src/test/java/testPackage01/Test_dbConnection.java
+++ src/test/java/testPackage01/Test_dbConnection.java
@@ -4,7 +4,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.shaft.support.DBActions;
@@ -16,6 +15,8 @@ public class Test_dbConnection {
 
 	@Test
 	public void test_mySQLConnection() {
+		dbActions = new DBActions("mysql","72.55.136.25","3306","menna","incorta","incorta_123");
+		dbActions.createConnection();
 		QR= dbActions.executeSelectQuery("select * from DASHBOARD;");
 		
 		try {
@@ -27,18 +28,28 @@ public class Test_dbConnection {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-
 	}
-
-	@BeforeClass // Set-up method, to be run once before the first test
-	public void beforeClass() {
-		dbActions = new DBActions("mysql","72.55.136.25","3306","menna","incorta","incorta_123");
+	
+	@Test
+    public void test_SQLServerConnection() {
+		dbActions = new DBActions("sqlserver","72.55.136.25","1432","salesdb","sa","1nc0rta_123");
 		dbActions.createConnection();
-	}
+		QR= dbActions.executeSelectQuery("select * from abdelsalam.dbo.automation_table");
+		
+		try {
+			while(QR.next())
+			{
+				System.out.print(QR.getString(2)); //or rs.getString("column name");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+    }
 
 	@AfterClass // Tear-down method, to be run once after the last test
-	public void afterClass() {
+	public void afterMethod() {
 		dbActions.closeTheConnection();
 	}
 }
