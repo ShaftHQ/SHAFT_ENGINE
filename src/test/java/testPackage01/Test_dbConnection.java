4d186ef4c2d7f14093b4aad7f9b9518c3b376592
mennamagedm <menna.maged@incorta.com>
2019-06-18 17:07:38 +0200

    apply review comments

--- src/test/java/testPackage01/Test_dbConnection.java
+++ src/test/java/testPackage01/Test_dbConnection.java
@@ -3,7 +3,6 @@ package testPackage01;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 
 import com.shaft.support.DBActions;
@@ -13,10 +12,9 @@ public class Test_dbConnection {
 	DBActions dbActions;
 	ResultSet queryResult;
 
-	//@Test
+	@Test
 	public void test_mySQLConnection() {
 		dbActions = new DBActions("mysql","72.55.136.25","3306","menna","incorta","incorta_123");
-		dbActions.createConnection();
 		queryResult= dbActions.executeSelectQuery("select * from DASHBOARD;");
 		
 		try {
@@ -29,10 +27,9 @@ public class Test_dbConnection {
 		}
 	}
 	
-	//@Test
+	@Test
     public void test_SQLServerConnection() {
 		dbActions = new DBActions("sqlserver","72.55.136.25","1432","salesdb","sa","1nc0rta_123");
-		dbActions.createConnection();
 		queryResult= dbActions.executeSelectQuery("select * from abdelsalam.dbo.automation_table");
 		
 		try {
@@ -48,7 +45,6 @@ public class Test_dbConnection {
 	@Test
     public void test_PostgreSQLConnection() {
 		dbActions = new DBActions("postgresql","72.55.136.25","5432","demo","dev","dev_incorta");
-		dbActions.createConnection();
 		queryResult= dbActions.executeSelectQuery("select * from account");
 		
 		try {
@@ -61,8 +57,4 @@ public class Test_dbConnection {
 		}
     }
 
-	@AfterClass // Tear-down method, to be run once after the last test
-	public void afterMethod() {
-		dbActions.closeTheConnection();
-	}
 }
