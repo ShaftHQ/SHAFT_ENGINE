de08bfbba2cf7584e2562251926172e1f22d1325
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2019-07-01 14:25:29 +0200

    refactoring database actions to follow the pattern of api/gui/cli
    .DatabaseActions:
    ..removed commented code
    ..removed unused method
    ..changed resultSet object to be more flexible
    ..increased logging and reporting
    ..added ability to attach query execution result by default
    ..created getStringFromResultSet(ResultSet resultSet)
    .execution.properties:
    ..added databaseLoginTimeout, databaseNetworkTimeout &
    databaseQueryTimeout
    .pom.xml:
    ..version upgrade
    .ReportManager:
    ..updated log(Exception e) to log(Throwable t)

--- src/test/java/testPackage01/Test_dbConnection.java
+++ src/test/java/testPackage01/Test_dbConnection.java
@@ -1,12 +1,11 @@
 package testPackage01;
 
 import java.sql.ResultSet;
-import java.sql.SQLException;
 
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import com.shaft.db.DatabaseActions;
+import com.shaft.validation.Assertions;
 
 public class Test_dbConnection {
 
@@ -18,13 +17,8 @@ public class Test_dbConnection {
 	dbActions = new DatabaseActions("mysql", "72.55.136.25", "3306", "menna", "incorta", "incorta_123");
 	queryResult = dbActions.executeSelectQuery("select * from DASHBOARD;");
 
-	try {
-	    while (queryResult.next()) {
-		System.out.print(queryResult.getString(2)); // or rs.getString("column name");
-	    }
-	} catch (SQLException e) {
-	    e.printStackTrace();
-	}
+	Assertions.assertEquals("C87184_sql_spark_HAVING Dashboard", DatabaseActions.getResultSetValue(queryResult), 3,
+		true);
     }
 
     @Test
@@ -32,13 +26,7 @@ public class Test_dbConnection {
 	dbActions = new DatabaseActions("sqlserver", "72.55.136.25", "1432", "salesdb", "sa", "1nc0rta_123");
 	queryResult = dbActions.executeSelectQuery("select * from abdelsalam.dbo.automation_table");
 
-	try {
-	    while (queryResult.next()) {
-		System.out.print(queryResult.getString(2)); // or rs.getString("column name");
-	    }
-	} catch (SQLException e) {
-	    e.printStackTrace();
-	}
+	Assertions.assertEquals("AutomationTest", DatabaseActions.getResultSetValue(queryResult), 3, true);
     }
 
     @Test
@@ -46,18 +34,6 @@ public class Test_dbConnection {
 	dbActions = new DatabaseActions("postgresql", "72.55.136.25", "5432", "demo", "dev", "dev_incorta");
 	queryResult = dbActions.executeSelectQuery("select * from account");
 
-	try {
-	    while (queryResult.next()) {
-		System.out.print(queryResult.getString(2)); // or rs.getString("column name");
-	    }
-	} catch (SQLException e) {
-	    e.printStackTrace();
-	}
+	Assertions.assertEquals("ahmed", DatabaseActions.getResultSetValue(queryResult), 3, true);
     }
-
-    @AfterMethod
-    public void afterMethod() {
-//	dbActions.closeConnection();
-    }
-
 }
