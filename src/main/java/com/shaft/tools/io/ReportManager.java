335e8ac32a29f11636b33c50c105dce06515b40e
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2020-06-23 09:06:15 +0200

    improving code quality

--- src/main/java/com/shaft/tools/io/ReportManager.java
+++ src/main/java/com/shaft/tools/io/ReportManager.java
@@ -406,7 +406,7 @@ public class ReportManager {
 
     public static void setTestCaseName(String scenarioName) {
         Allure.getLifecycle().updateTestCase(testResult -> testResult.setName(scenarioName));
-        if (!featureName.equals("")) {
+        if (!"".equals(featureName)) {
             Allure.getLifecycle().updateTestCase(testResult -> testResult.setFullName(featureName + ": " + scenarioName));
         }
     }
