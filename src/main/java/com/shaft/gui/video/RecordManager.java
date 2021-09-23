55b12a19d9298f9d570c7a05afead6175329398b
sys123-data <68052609+sys123-data@users.noreply.github.com>
2021-09-23 16:38:36 +0300

    Android Video Encoding Optimization

--- src/main/java/com/shaft/gui/video/RecordManager.java
+++ src/main/java/com/shaft/gui/video/RecordManager.java
@@ -7,6 +7,7 @@ import com.shaft.driver.DriverFactoryHelper;
 import com.shaft.tools.io.ReportManager;
 import com.shaft.tools.io.ReportManagerHelper;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidStartScreenRecordingOptions;
 import io.appium.java_client.ios.IOSDriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
@@ -21,6 +22,7 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.time.Duration;
 import java.util.Base64;
 
 import static com.automation.remarks.video.RecordingUtils.doVideoProcessing;
@@ -44,7 +46,7 @@ public class RecordManager {
             videoDriver.set(driver);
             try {
                 if (driver instanceof AndroidDriver androidDriver) {
-                    androidDriver.startRecordingScreen();
+                    androidDriver.startRecordingScreen(new AndroidStartScreenRecordingOptions().withVideoSize("540x960").withBitRate(2000000).withTimeLimit(Duration.parse("PT30M")));
                 } else if (driver instanceof IOSDriver iosDriver) {
                     iosDriver.startRecordingScreen();
                 }
@@ -102,26 +104,10 @@ public class RecordManager {
         File source = new File(pathToRecording);
         File target = new File(pathToRecording.replace("avi", "mp4"));
         try {
-            // feel free to tinker around with this method until you reach the optimal configuration.
-            // if you want to make anything configurable by the user you can follow these simple steps:
-            // 1. go to src/main/resorces/defaultProperties/video.properties
-            // 2. create new properties for the variables that you would like to be configurable and add default values there equal to the ones you use here in the code
-            // 3. use this line System.getProperty("{PROPERTYNAME}").trim() to read any string properties and feel free to cast to int or boolean as needed
-            // 4. this file and video settings are currently not existing in the Configuration Manager UI, we can add them at a later step, but for now users can change values in this properties file manually
 
             AudioAttributes audio = new AudioAttributes();
-            //todo: optionally disable audio support (disabled by default)
             audio.setCodec("libvorbis");
-//            audio.setBitRate(64);
-//            audio.setCodec("AAC");
-//            audio.setChannels(1);
             VideoAttributes video = new VideoAttributes();
-            //todo: reduce framerate to 24 / make it configurable
-            video.setFrameRate(30);
-            //todo: set bitrate to 480
-//            video.setBitRate(480);
-            //todo: set bitrate to vp9 /av1
-//            video.setCodec("vp9");
             EncodingAttributes attrs = new EncodingAttributes();
             attrs.setOutputFormat("mp4");
             attrs.setAudioAttributes(audio);
