457463c4b1fc8c34ed9ee45d081901b474768a2d
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2021-11-03 23:59:47 +0200

    fixes and enhancements
    - fix broken test
    - version bump for new release

--- src/test/java/testPackage01/Test_JsonActions.java
+++ src/test/java/testPackage01/Test_JsonActions.java
@@ -18,9 +18,9 @@ public class Test_JsonActions {
     @Test
     public void f() throws IOException, ParseException, JSONException {
         String expectedResponse = new String(
-                Files.readAllBytes(Paths.get(System.getProperty("testDataFolderPath") + "subFolder/JsonFileTest2.json")));
+                Files.readAllBytes(Paths.get(System.getProperty("testDataFolderPath") + "JsonFileTest2.json")));
         String actualResponse = new String(
-                Files.readAllBytes(Paths.get(System.getProperty("testDataFolderPath") + "subFolder/JsonFileTest.json")));
+                Files.readAllBytes(Paths.get(System.getProperty("testDataFolderPath") + "JsonFileTest.json")));
 
         JSONObject expectedJsonObject = (JSONObject) (new JSONParser()).parse(expectedResponse);
         JSONObject actualJsonObject = (JSONObject) (new JSONParser()).parse(actualResponse);
