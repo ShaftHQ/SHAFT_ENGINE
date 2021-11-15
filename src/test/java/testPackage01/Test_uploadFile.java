80505e0b5ca5b09b03bb7486184e0d230163683d
MamdouhMansour <39811691+MamdouhMansour@users.noreply.github.com>
2021-11-15 22:30:39 +0200

    Enhance typeFileLocationForUpload To Auto Detect Relative Path (#449)
    * Enhance typeFileLocationForUpload To Auto Detect Relative Path, Remove Deprecated Methods From Test
    
    * Remove Deprecated Methods From Test
    
    Co-authored-by: Mamdouh_Mansour <mamdoh@terkwaz.com>

--- src/test/java/testPackage01/Test_uploadFile.java
+++ src/test/java/testPackage01/Test_uploadFile.java
@@ -1,10 +1,10 @@
 package testPackage01;
 
 import com.shaft.cli.FileActions;
+import com.shaft.driver.DriverFactory;
 import com.shaft.gui.browser.BrowserActions;
-import com.shaft.gui.browser.BrowserFactory;
 import com.shaft.gui.element.ElementActions;
-import com.shaft.validation.Assertions;
+import com.shaft.validation.Validations;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
@@ -17,21 +17,21 @@ public class Test_uploadFile {
     @Test
     public void uploadFile_visibleUploadInput() {
         BrowserActions.navigateToURL(driver.get(), "http://demo.guru99.com/test/upload/");
-        ElementActions.typeFileLocationForUpload(driver.get(), By.id("uploadfile_0"), FileActions.getAbsolutePath("src/main/resources/images/", "shaft.png"));
+        ElementActions.typeFileLocationForUpload(driver.get(), By.id("uploadfile_0"), "src/main/resources/images/shaft.png");
         new ElementActions(driver.get()).click(By.id("terms")).click(By.id("submitbutton"));
-        Assertions.assertElementAttribute(driver.get(), By.id("res"), "Text", "1 file", Assertions.AssertionComparisonType.CONTAINS, Assertions.AssertionType.POSITIVE);
+        Validations.assertThat().element(driver.get(), By.id("res")).attribute("Text").contains("1 file").perform();
     }
 
     @Test
     public void uploadFile_invisibleUploadInput() {
         BrowserActions.navigateToURL(driver.get(), "https://fineuploader.com/demos.html#gallery-view");
         ElementActions.typeFileLocationForUpload(driver.get(), By.xpath("//div[@id='fine-uploader-gallery']//input[@type='file']"), FileActions.getAbsolutePath("src/main/resources/images/", "shaft.png"));
-        Assertions.assertElementAttribute(driver.get(), By.xpath("//div[@id='fine-uploader-gallery']//div[@class='qq-thumbnail-wrapper']/img"), "src", "data:image/png;base64", Assertions.AssertionComparisonType.CONTAINS, Assertions.AssertionType.POSITIVE);
+        Validations.assertThat().element(driver.get(), By.xpath("//div[@id='fine-uploader-gallery']//div[@class='qq-thumbnail-wrapper']/img")).attribute("src").contains("data:image/png;base64").perform();
     }
 
     @BeforeMethod
     public void beforeMethod() {
-        driver.set(BrowserFactory.getBrowser());
+        driver.set(DriverFactory.getDriver());
     }
 
     @AfterMethod
