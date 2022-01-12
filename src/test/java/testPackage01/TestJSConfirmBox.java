b27d2159cbcdfa4072b63debd805db68e0534ba2
Ahmed Madyan <85081446+AhmedMadyan01@users.noreply.github.com>
2022-01-12 17:44:01 +0200

    Alert actions (#472)
    * Handling browser alerts
    
    - AlertActions class added for handling JS browser alerts
    - performAlertAction method added to ElementActions class
    - Test case added in separate classes for each type of alert [Box, Confirm, Prompt]
    
    * UPDATED annotations
    
    Changed @BeforeTest to @BeforeClass
    Changed @AfterTest to @AfterClass
    
    * Request changes applied
    
    - Removed required parameter [driver] to call any Method in the AlertActions class to make it usable only by performAlertAction.
    - Changed the wait method into private method to make it accessible throw only the methods in the AlertActions class.
    - Implemented [Pass / Fail] Actions into AlertActions Class.
    - Replaced System.out.println with ReportManager.logDiscrete.
    - Fixed Assertion method in the test package.

---
+++ src/test/java/testPackage01/TestJSConfirmBox.java
@@ -0,0 +1,59 @@
+package testPackage01;
+
+import com.shaft.driver.DriverFactory;
+import com.shaft.gui.browser.BrowserActions;
+import com.shaft.gui.element.ElementActions;
+import com.shaft.tools.io.ReportManager;
+import com.shaft.validation.ValidationEnums;
+import com.shaft.validation.Validations;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TestJSConfirmBox {
+
+    private static final By JS_ConfirmAlert = By.xpath("//button[contains(text(),'Click for JS Confirm')]");
+    private static final By JS_ResultText = By.id("result");
+    private static WebDriver driver;
+
+    @BeforeClass
+    public void navigateToJSAlertPage() {
+        driver = DriverFactory.getDriver();
+        BrowserActions.navigateToURL(driver, "http://the-internet.herokuapp.com/javascript_alerts");
+    }
+
+    @AfterClass
+    public void tearDownDriver() {
+        BrowserActions.closeCurrentWindow(driver);
+    }
+
+    @Test
+    public void dismissAlert() {
+        ElementActions.click(driver, JS_ConfirmAlert);
+        ElementActions.performAlertAction(driver).dismissAlert();
+    }
+
+    @Test(dependsOnMethods = "dismissAlert")
+    public void assertOnCancelAlertResultText() {
+        Validations.assertThat().element(driver, JS_ResultText).attribute(ValidationEnums.ElementAttribute.TEXT).isEqualTo("You clicked: Cancel").perform();
+    }
+
+    @Test(dependsOnMethods = "assertOnCancelAlertResultText")
+    public void getAlertText() {
+        ElementActions.click(driver, JS_ConfirmAlert);
+        ReportManager.logDiscrete("Alert text is: [" + ElementActions.performAlertAction(driver).getAlertText() + "]");
+        Validations.assertThat().object(ElementActions.performAlertAction(driver).getAlertText()).isEqualTo("I am a JS Confirm").perform();
+    }
+
+    @Test(dependsOnMethods = "getAlertText")
+    public void acceptAlert() {
+        ElementActions.performAlertAction(driver).acceptAlert();
+    }
+
+    @Test(dependsOnMethods = "acceptAlert")
+    public void assertOnConfirmAlertResultText() {
+        Validations.assertThat().element(driver, JS_ResultText).attribute(ValidationEnums.ElementAttribute.TEXT).isEqualTo("You clicked: Ok").perform();
+    }
+}
