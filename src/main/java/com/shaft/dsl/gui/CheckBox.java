3134445da3609a5489eb4dccd6aab3a399757b73
MhmdElGazzar <77445441+MhmdElGazzar@users.noreply.github.com>
2022-02-07 19:58:39 +0200

    dsl core components v1 (#509)
    * dsl core components v1
    
    * setting pscksge
    
    * add packages
    
    * commit tests and code review points
    
    * Update testDsl.java
    
    * package rename

---
+++ src/main/java/com/shaft/dsl/gui/CheckBox.java
@@ -0,0 +1,10 @@
+package com.shaft.dsl.gui;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CheckBox extends RadioButton{
+    public CheckBox(WebDriver driver, By buttonLocator, By labelLoctor) {
+        super(driver, buttonLocator, labelLoctor);
+    }
+}
