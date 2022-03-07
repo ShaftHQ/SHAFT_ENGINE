fd0437e9342a0207ac5473beca0d719ed39497c6
Reham <rihammostafa005@gmail.com>
2022-03-07 09:37:13 +0200

    Revert "Reading json file all content as string"
    This reverts commit b80bf422d04dc4115688c14125709f315796436c.

--- src/test/java/testPackage01/Test_JSONFileManager.java
+++ src/test/java/testPackage01/Test_JSONFileManager.java
@@ -4,11 +4,6 @@ import com.shaft.tools.io.JSONFileManager;
 import com.shaft.validation.Assertions;
 import org.testng.annotations.Test;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
-
 public class Test_JSONFileManager {
     JSONFileManager testDataJSON;
 
@@ -16,12 +11,5 @@ public class Test_JSONFileManager {
     public void readStringFromJson() {
         testDataJSON = new JSONFileManager(System.getProperty("testDataFolderPath") + "simpleJSON.json");
         Assertions.assertEquals("Mohab Mohie", testDataJSON.getTestData("x.name"));
-    }
-    @Test
-    public void readJsonFileAsString() throws IOException{
-        testDataJSON = new JSONFileManager(System.getProperty("testDataFolderPath") + "simpleJSON.json");
-        String Excepted = Files.readString(Paths.get(System.getProperty("testDataFolderPath") + "simpleJSON.txt"));
-
-        Assertions.assertEquals(Excepted,testDataJSON.readJsonFileAsString());
     }
 }
