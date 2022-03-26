f3026cc5b5ed2f2653fd6f9acb6de2b9e3216567
Mahmoud ElSharkawy <mahmoud.elsharkawy1994@gmail.com>
2022-03-26 19:51:58 +0200

    Add getResponseJSONValueFromList method (#566)
    * Added getResponseJSONValueFromList method
    
    * Added a Test Case for the getResponseJSONValueFromList method
    
    * Enhance the documentation for getResponseJSONValueFromList method
    
    * Formatting for getResponseJSONValueFromList method

--- src/main/java/com/shaft/api/RestActions.java
+++ src/main/java/com/shaft/api/RestActions.java
@@ -232,6 +232,33 @@ public class RestActions implements ShaftDriver {
         }
     }
 
+    /**
+     * Extracts a string value from an object of a list by reference of another attribute inside of the same object
+     *
+     * @param response                 The target API response object
+     * @param jsonPathToList           The JSON path to the list of object inside of the full response
+     * @param jsonPathToValueNeeded    The JSON path to the attribute value you need to extract inside an object from the list. for example: id
+     * @param jsonPathToValueReference The JSON path that refers to the needed attribute value inside an object from the list. for example: username
+     * @param valueReference           The attribute value of the reference JSON path
+     * @return A string value from the object of the list
+     */
+    public static String getResponseJSONValueFromList(Response response, String jsonPathToList, String jsonPathToValueNeeded,
+                                                      String jsonPathToValueReference, String valueReference) {
+        List<Object> list = getResponseJSONValueAsList(response, jsonPathToList);
+        String value = "";
+        for (Object res : list) {
+            if (getResponseJSONValue(res, jsonPathToValueReference).equals(valueReference)) {
+                value = getResponseJSONValue(res, jsonPathToValueNeeded);
+            }
+        }
+        if (value.equals("")) {
+            failAction("Can't find the reference value [" + valueReference + "] in the list with the [" + jsonPathToValueReference + "] JSON Path");
+        } else {
+            passAction(value);
+        }
+        return value;
+    }
+
     public static String getResponseXMLValue(Response response, String xmlPath) {
         String searchPool = "";
         try {
