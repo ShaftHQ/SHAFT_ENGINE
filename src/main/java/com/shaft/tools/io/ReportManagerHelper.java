4d2e62bb0277869dddacc5b7acdb172d7c61cbcd
Mahmoud El-Sharkawy <mahmoud.elsharkawy1994@gmail.com>
2022-08-23 20:59:26 +0200

    Upload report artifact test #687

--- src/main/java/com/shaft/tools/io/ReportManagerHelper.java
+++ src/main/java/com/shaft/tools/io/ReportManagerHelper.java
@@ -185,14 +185,10 @@ public class ReportManagerHelper {
         System.setProperty("disableLogging", "true");
 //        boolean discreteLoggingState = isDiscreteLogging();
         allureResultsFolderPath = System.getProperty("allureResultsFolderPath").trim();
-        if (System.getProperty("executionAddress").trim().equals("local")
-                || (System.getProperty("mobile_platformName") != null && !System.getProperty("mobile_platformName").trim().equals(""))) {
 //            setDiscreteLogging(true);
-            cleanAllureResultsDirectory();
-            downloadAndExtractAllureBinaries();
-            writeGenerateReportShellFilesToProjectDirectory();
-
-        }
+        cleanAllureResultsDirectory();
+        downloadAndExtractAllureBinaries();
+        writeGenerateReportShellFilesToProjectDirectory();
         writeEnvironmentVariablesToAllureResultsDirectory();
 
 //        setDiscreteLogging(discreteLoggingState);
