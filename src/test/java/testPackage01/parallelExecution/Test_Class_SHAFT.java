aaa1c8dd37b44d1da21952ab0ca503f0859bd068
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2022-09-01 19:39:21 +0200

    clean SHAFT test (old syntax)

--- src/test/java/testPackage01/parallelExecution/Test_Class_SHAFT.java
+++ src/test/java/testPackage01/parallelExecution/Test_Class_SHAFT.java
@@ -12,7 +12,6 @@ import java.nio.file.Paths;
 public class Test_Class_SHAFT {
     private static final String html = Paths.get("src/test/resources/testDataFiles/test.html").toUri().toString();
     private static ThreadLocal<SHAFT.GUI.WebDriver> driver = new ThreadLocal<>();
-//    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
     private static final By firstName_input = By.id("fname");
     private static final By lastName_input = By.id("lname");
@@ -61,9 +60,6 @@ public class Test_Class_SHAFT {
     }
 
     private void commonSteps(String firstName, String lastName){
-        //native selenium works flawlessly and is extremely fast with consistent 100% success rate
-        //fluent shaft seems to be broken (success rate 50%)
-        //regular shaft seems to be also broken, albeit seemingly more stable (success rate 90% -> dropped to 50%)
 
         BrowserActions.navigateToURL(driver.get().getDriver(), html);
         ElementActions.type(driver.get().getDriver(), firstName_input, firstName);
@@ -76,45 +72,11 @@ public class Test_Class_SHAFT {
 //                .type(lastName_input, lastName)
 //                .click(submit_button);
 //        driver.get().assertThat().element(output_label).text().contains("fname="+firstName+"&lname="+lastName).perform();
-
-
-//        driver.get().navigate().to(html);
-//        driver.get().findElement(firstName_input).clear();
-//        driver.get().findElement(firstName_input).sendKeys(firstName);
-//        driver.get().findElement(lastName_input).clear();
-//        driver.get().findElement(lastName_input).sendKeys(lastName);
-//        driver.get().findElement(submit_button).click();
-//        var actualText = driver.get().findElement(output_label).getText();
-//        var expectedText = "fname="+firstName+"&lname="+lastName;
-//        ReportManager.log("Actual Text: "+ actualText);
-//        ReportManager.log("Expected Text: "+ expectedText);
-//        Assert.assertTrue(actualText.contains(expectedText));
-    }
-
-    @BeforeClass
-    public void beforeClass(){
-//        System.setProperty("generateExtentReports", "false");
-//        System.setProperty("setVerbose", "10");
-//        System.setProperty("setParallel", "METHODS");
-//        System.setProperty("setThreadCount", "4");
-//        System.setProperty("executionAddress", "dockerized");
-
-
-    }
-
-    @AfterClass
-    public void afterClass(){
-//        System.setProperty("generateExtentReports", "true");
-//        System.setProperty("setVerbose", "1");
-//        System.setProperty("setParallel", "NONE");
-//        System.setProperty("setThreadCount", "1");
-//        System.setProperty("executionAddress", "local");
     }
 
     @BeforeMethod
     public void beforeMethod() {
         driver.set(new SHAFT.GUI.WebDriver());
-//        driver.set(WebDriverManager.chromedriver().create());
     }
 
     @AfterMethod
