66827518907fb4983cb321f4c7f5335c7938f07b
Mahmoud El-Sharkawy <mahmoud.elsharkawy1994@gmail.com>
2022-09-23 22:25:46 +0200

    Add API response body and time direct validations tests

--- src/test/java/testPackage01/SHAFTWizard/Test_Wizard_API.java
+++ src/test/java/testPackage01/SHAFTWizard/Test_Wizard_API.java
@@ -5,20 +5,21 @@ import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
+import java.util.List;
 
 @SuppressWarnings("unchecked")
 public class Test_Wizard_API {
     SHAFT.API driver;
 
     @Test
-    public void test() {
+    public void test_get_matchesSchema() {
         driver = new SHAFT.API("http://api.zippopotam.us/");
         driver.get("us/90210").perform();
         driver.assertThatResponse().matchesSchema("schema.json").perform();
     }
 
     @Test
-    public void test_post_withbody_string() {
+    public void test_post_withBody_string() {
         driver = new SHAFT.API("https://reqres.in/");
         String body = """
                 {
@@ -30,7 +31,7 @@ public class Test_Wizard_API {
     }
 
     @Test
-    public void test_post_withbody_hashMap() {
+    public void test_post_withBody_hashMap() {
         driver = new SHAFT.API("https://reqres.in/");
         HashMap body = new HashMap<>();
         body.put("name", "morpheus");
@@ -40,7 +41,7 @@ public class Test_Wizard_API {
     }
 
     @Test
-    public void test_post_withbody_JSONObject() {
+    public void test_post_withBody_JSONObject() {
         driver = new SHAFT.API("https://reqres.in/");
         HashMap body = new HashMap<>();
         body.put("name", "morpheus");
@@ -51,7 +52,7 @@ public class Test_Wizard_API {
     }
 
     @Test
-    public void test_post_withbody_JSONObject_simple() {
+    public void test_post_withBody_JSONObject_simple() {
         driver = new SHAFT.API("https://reqres.in/");
         HashMap body = new HashMap<>();
         body.put("name", "morpheus");
@@ -69,7 +70,7 @@ public class Test_Wizard_API {
     }
 
     @Test
-    public void test_assertResponsedoesNotEqualFileContent() {
+    public void test_assertResponseDoesNotEqualFileContent() {
         driver = new SHAFT.API("http://api.zippopotam.us/");
         driver.get("us/90210").perform();
         driver.assertThatResponse().doesNotEqualFileContent("test_assertResponseEqualsIgnoringOrder.json").perform();
@@ -101,6 +102,65 @@ public class Test_Wizard_API {
         driver = new SHAFT.API("https://jsonplaceholder.typicode.com");
         driver.get("/todos").perform();
         driver.assertThatResponse().extractedJsonValue("$[1].completed").isEqualTo("false").perform();
+    }
+
+    /////////////////////////  Getters  /////////////////////////
+    @Test
+    public void test_getResponseBody() {
+        driver = new SHAFT.API("http://api.zippopotam.us/");
+        driver.get("us/90210").perform();
+        String body = driver.getResponseBody();
+        SHAFT.Validations.assertThat().object(body).contains("Beverly Hills").perform();
+    }
+
+    @Test
+    public void test_getResponseStatusCode() {
+        driver = new SHAFT.API("http://api.zippopotam.us/");
+        driver.get("us/90210").perform();
+        int statusCode = driver.getResponseStatusCode();
+        SHAFT.Validations.assertThat().number(statusCode).isEqualTo(200).perform();
+    }
+
+    @Test
+    public void test_getResponseTime() {
+        driver = new SHAFT.API("http://api.zippopotam.us/");
+        driver.get("us/90210").perform();
+        long responseTime = driver.getResponseTime();
+        SHAFT.Validations.verifyThat().number(responseTime).isGreaterThanOrEquals(1.1).perform();
+        SHAFT.Validations.verifyThat().number(responseTime).isLessThanOrEquals(100000).perform();
+    }
+
+    @Test
+    public void test_getResponseJSONValue() {
+        driver = new SHAFT.API("https://jsonplaceholder.typicode.com");
+        driver.get("/users").perform();
+        String value = driver.getResponseJSONValue("$[?(@.name=='Ervin Howell')].address.street");
+        SHAFT.Validations.assertThat().object(value).isEqualTo("Victor Plains").perform();
+    }
+
+    @Test
+    public void test_getResponseJSONValueAsList() {
+        driver = new SHAFT.API("https://jsonplaceholder.typicode.com");
+        driver.get("/todos").perform();
+        List<Object> completedList = driver.getResponseJSONValueAsList("$[?(@.completed==true)].completed");
+        for (Object completed : completedList) {
+            SHAFT.Validations.verifyThat().object(completed.toString()).isEqualTo("true").perform();
+        }
+    }
+
+    @Test
+    public void test_validationWizard_getResponseBody() {
+        driver = new SHAFT.API("http://api.zippopotam.us/");
+        driver.get("us/90210").perform();
+        driver.assertThatResponse().body().contains("Beverly Hills").perform();
+    }
+
+    @Test
+    public void test_validationWizard_getResponseTime() {
+        driver = new SHAFT.API("https://jsonplaceholder.typicode.com");
+        driver.get("/users").perform();
+        driver.verifyThatResponse().time().isGreaterThanOrEquals(1).perform();
+        driver.assertThatResponse().time().isLessThanOrEquals(100000.1).perform();
     }
 
 }
