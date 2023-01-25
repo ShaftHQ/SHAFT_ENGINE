501d34bdecf1319f2219acbe198fa9f3ba3c3e62
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2023-01-25 11:46:21 +0200

    fixes #834
    Signed-off-by: Mohab Mohie <Mohab.MohieElDeen@outlook.com>

---
+++ src/test/java/testPackage/mockedTests/ElementVisibilityTest.java
@@ -0,0 +1,66 @@
+package testPackage.mockedTests;
+
+import com.shaft.driver.SHAFT;
+import org.openqa.selenium.By;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ElementVisibilityTest {
+    ThreadLocal<SHAFT.GUI.WebDriver> driver = new ThreadLocal<>();
+    String testElement = "data:text/html,<script>var result;</script><button ${HIDDEN} alt='Google' onclick='result=\"Clicked\"'>Go</button>";
+    By locator = SHAFT.GUI.Locator.hasTagName("button").hasAttribute("alt", "Google").build();
+
+    @Test
+    public void elementHiddenAndExpectedToBeHidden() {
+        driver.get().browser().navigateToURL(testElement.replace("${HIDDEN}", "hidden"));
+        driver.get().element().waitToBeInvisible(locator);
+        driver.get().assertThat().element(locator).isHidden().perform();
+    }
+
+    @Test
+    public void elementVisibleAndExpectedToBeVisible() {
+        driver.get().browser().navigateToURL(testElement.replace("${HIDDEN}", ""));
+        driver.get().element().waitToBeReady(locator);
+        driver.get().assertThat().element(locator).isVisible().perform();
+    }
+
+    @Test(expectedExceptions = {AssertionError.class}, expectedExceptionsMessageRegExp = ".*FAILED.*")
+    public void elementHiddenAndExpectedToBeVisible() {
+        driver.get().browser().navigateToURL(testElement.replace("${HIDDEN}", "hidden"));
+        driver.get().element().waitToBeInvisible(locator);
+        driver.get().assertThat().element(locator).isVisible().perform();
+    }
+
+    @Test(expectedExceptions = {AssertionError.class}, expectedExceptionsMessageRegExp = ".*FAILED.*")
+    public void elementVisibleAndExpectedToBeHidden() {
+        driver.get().browser().navigateToURL(testElement.replace("${HIDDEN}", ""));
+        driver.get().element().waitToBeReady(locator);
+        driver.get().assertThat().element(locator).isHidden().perform();
+    }
+
+    @Test(expectedExceptions = {AssertionError.class}, expectedExceptionsMessageRegExp = ".*Failed to Wait for element to be present.*")
+    public void elementDoesntExistAndExpectedToBeVisible() {
+        driver.get().browser().navigateToURL(testElement.replace("${HIDDEN}", "hidden"));
+        driver.get().element().waitToBeInvisible(By.id("bla"));
+        driver.get().assertThat().element(locator).isVisible().perform();
+    }
+
+    @Test(expectedExceptions = {AssertionError.class}, expectedExceptionsMessageRegExp = ".*Failed to Wait for element to be present.*")
+    public void elementDoesntExistAndExpectedToBeHidden() {
+        driver.get().browser().navigateToURL(testElement.replace("${HIDDEN}", ""));
+        driver.get().element().waitToBeReady(By.id("bla"));
+        driver.get().assertThat().element(locator).isHidden().perform();
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        driver.set(new SHAFT.GUI.WebDriver());
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        driver.get().quit();
+        driver.remove();
+    }
+}
