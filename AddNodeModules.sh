696d1eb7d8ecc39dc1e6c1e892066cb8bdd3f8c4
VF-ROOT\AbbasM3 <mohab.abbas@vodafone.com>
2023-03-16 14:53:20 +0200

    Add Latest Files

--- AddNodeModules.sh
+++ AddNodeModules.sh
@@ -4,9 +4,5 @@ npm install -g lighthouse
 npm install -g puppeteer
 npm install fs
 npm install open
-<<<<<<< HEAD
 npm install optimist
-=======
 npm install optimist
-
->>>>>>> 7eafd7359e7a20252c7afe243ff589c232b773d2
