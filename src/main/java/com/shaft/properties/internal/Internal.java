9795168e015c39668c51e42dc687caf0c4810776
MohabMohie <Mohab.MohieElDeen@outlook.com>
2023-04-19 15:31:54 +0200

    20230419
    Signed-off-by: MohabMohie <Mohab.MohieElDeen@outlook.com>

--- src/main/java/com/shaft/properties/internal/Internal.java
+++ src/main/java/com/shaft/properties/internal/Internal.java
@@ -10,7 +10,7 @@ import org.aeonbits.owner.Config.Sources;
 })
 public interface Internal extends EngineProperties {
     @Key("shaftEngineVersion")
-    @DefaultValue("SHAFT Engine v7.2.20230418")
+    @DefaultValue("SHAFT Engine v7.2.20230419")
     String shaftEngineVersion();
 
     @Key("watermarkImagePath")
