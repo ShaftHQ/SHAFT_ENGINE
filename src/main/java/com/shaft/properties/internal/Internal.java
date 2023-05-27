ec21d98f7e462a645ac3942b08570eee88a459e4
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2023-05-27 17:36:29 +0300

    1143 feature request prepare for stable major release (#1144)
    * removing deprecated methods
    
    Signed-off-by: Mohab Mohie <Mohab.MohieElDeen@outlook.com>
    
    * removing beta tags for stable new features
    
    Signed-off-by: Mohab Mohie <Mohab.MohieElDeen@outlook.com>
    
    * version bump
    
    Signed-off-by: Mohab Mohie <Mohab.MohieElDeen@outlook.com>
    
    ---------
    
    Signed-off-by: Mohab Mohie <Mohab.MohieElDeen@outlook.com>

--- src/main/java/com/shaft/properties/internal/Internal.java
+++ src/main/java/com/shaft/properties/internal/Internal.java
@@ -10,7 +10,7 @@ import org.aeonbits.owner.Config.Sources;
 })
 public interface Internal extends EngineProperties {
     @Key("shaftEngineVersion")
-    @DefaultValue("SHAFT Engine v7.2.20230525_BETA")
+    @DefaultValue("SHAFT Engine v7.2.20230527")
     String shaftEngineVersion();
 
     @Key("watermarkImagePath")
