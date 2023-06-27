ed4c6499b3d63aa74afd9f90b06225a49347a2d3
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2023-06-27 14:57:00 +0300

    minor junit enhancements
    Signed-off-by: Mohab Mohie <Mohab.MohieElDeen@outlook.com>

---
+++ src/main/java/com/shaft/listeners/internal/JunitListenerHelper.java
@@ -0,0 +1,25 @@
+package com.shaft.listeners.internal;
+
+import com.shaft.tools.io.internal.ReportManagerHelper;
+import org.junit.platform.launcher.TestIdentifier;
+
+public class JunitListenerHelper {
+    private static final ThreadLocal<String> testName = new ThreadLocal<>();
+
+    public static synchronized void setTestName(TestIdentifier testIdentifier) {
+        testName.set(testIdentifier.getDisplayName());
+    }
+
+    public static void logTestInformation(TestIdentifier testIdentifier) {
+        String className = ReportManagerHelper.getTestClassName();
+        String methodName = ReportManagerHelper.getTestMethodName();
+        String methodDescription = "";
+
+        if (testIdentifier.isTest()) {
+            ReportManagerHelper.logTestInformation(className, methodName, methodDescription);
+            ReportManagerHelper.extentReportsCreateTest(className + "." + methodName, methodDescription);
+        } else {
+            ReportManagerHelper.extentReportsReset();
+        }
+    }
+}
