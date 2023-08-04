cd19f056d3c4e528231eee8d3bc681c9fb589fc9
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2023-08-04 02:21:28 +0200

    stable release after a month in beta testing (#1215)
    Signed-off-by: Mohab Mohie <Mohab.MohieElDeen@outlook.com>

--- src/main/java/com/shaft/properties/internal/Internal.java
+++ src/main/java/com/shaft/properties/internal/Internal.java
@@ -10,7 +10,7 @@ import org.aeonbits.owner.Config.Sources;
 })
 public interface Internal extends EngineProperties {
     @Key("shaftEngineVersion")
-    @DefaultValue("7.3.20230707_BETA")
+    @DefaultValue("7.3.20230804")
     String shaftEngineVersion();
 
     @Key("watermarkImagePath")
