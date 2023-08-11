8f974e205a990d4ac1d24871eb67d2466594ea76
Ahmed Gamal Zanoon <65794900+zanoon2020@users.noreply.github.com>
2023-08-11 17:18:17 +0300

    Update BigPageActionsTest.java
    removing properties test

--- src/test/java/testPackage/BigPageActionsTest.java
+++ src/test/java/testPackage/BigPageActionsTest.java
@@ -67,41 +67,6 @@ public class BigPageActionsTest {
                 and special characters ...######$%^&&*!!""").perform();
     }
 
-    @Test
-    public void testSettingVisualProprtitesDuringRuntime() {
-        driver.element().type(By.id("et_pb_contact_name_0"), "TEST_NAME");
-        SHAFT.Properties.visuals.set().screenshotParamsWhenToTakeAScreenshot("Always");
-        SHAFT.Properties.visuals.set().screenshotParamsScreenshotType("Regular");
-                driver.element().clear(By.id("et_pb_contact_name_0"))
-                .type(By.id("et_pb_contact_name_0"), "TEST_NAME")
-                .type(By.id("et_pb_contact_email_0"), "email@email.email")
-                .type(By.id("et_pb_contact_message_0"), """
-                        This is a long message
-                        it will have line breaks
-                        and special characters ...######$%^&&*!!""");
-        SHAFT.Properties.visuals.set().screenshotParamsWhenToTakeAScreenshot("Never");
-        SHAFT.Properties.visuals.set().screenshotParamsScreenshotType("Regular");
-        driver.element().type(By.id("et_pb_contact_name_1"), "TEST_NAME")
-                .type(By.id("et_pb_contact_email_1"), "email@email.email")
-                .type(By.id("et_pb_contact_message_1"), """
-                        This is a long message
-                        it will have line breaks
-                        and special characters ...######$%^&&*!!""");
-        SHAFT.Properties.visuals.set().screenshotParamsWhenToTakeAScreenshot("ValidationPointsOnly");
-        SHAFT.Properties.visuals.set().screenshotParamsScreenshotType("element");
-        driver.element().type(By.id("et_pb_contact_name_2"), "TEST_NAME")
-                .type(By.id("et_pb_contact_email_2"), "email@email.email")
-                .type(By.id("et_pb_contact_message_2"), """
-                        This is a long message
-                        it will have line breaks
-                        and special characters ...######$%^&&*!!""")
-                .captureScreenshot(By.id("et_pb_contact_message_2"))
-                .and().browser().captureScreenshot();
-        driver.assertThat().element(By.id("et_pb_contact_message_2")).text().isEqualTo("""
-                This is a long message
-                it will have line breaks
-                and special characters ...######$%^&&*!!""").perform();
-    }
 
     @BeforeMethod
     public void beforeMethod() {
