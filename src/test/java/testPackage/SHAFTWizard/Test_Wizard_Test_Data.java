faa728912e82c654c05ffbafadbff45635b58284
MamdouhMansour <39811691+MamdouhMansour@users.noreply.github.com>
2023-08-23 16:17:12 +0300

    Feature/1240 map test dat from json file to java object (#1241)
    * Add GetTestDataAsJson To Map Json Test Data To Java Object
    
    * Add GetTestDataAsJson To Map Json Test Data To Java Object
    
    * Simplify Json Obj File
    
    * Move Pojo Class Out Of The Engine
    
    ---------
    
    Co-authored-by: Mamdouh-Mansour <mamdouh.mansour@meccasoft.com>

---
+++ src/test/java/testPackage/SHAFTWizard/Test_Wizard_Test_Data.java
@@ -0,0 +1,19 @@
+package testPackage.SHAFTWizard;
+
+import com.google.gson.Gson;
+import com.shaft.driver.SHAFT;
+import org.testng.annotations.Test;
+import objectclass.BodyObject;
+
+public class Test_Wizard_Test_Data {
+    private SHAFT.TestData.JSON testData;
+
+    @Test
+    public void test_mapping_test_data_to_java_object() {
+        testData = new SHAFT.TestData.JSON("jsonToJavaObjectTest.json");
+        Object obj = testData.getTestDataAsJson("data.jsonData");
+        Gson gson = new Gson();
+        BodyObject bodyObject = gson.fromJson(obj.toString(), BodyObject.class);
+        SHAFT.Validations.assertThat().object(bodyObject.getKey1()).isEqualTo("SHAFT");
+    }
+}
