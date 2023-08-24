18a032273ac1a0b019bc8977b0c1f3faadc81a0b
Mahmoud El-Sharkawy <mahmoud.elsharkawy1994@gmail.com>
2023-08-24 09:57:47 +0300

    Release 7.3.20230825

--- src/main/java/com/shaft/properties/internal/Internal.java
+++ src/main/java/com/shaft/properties/internal/Internal.java
@@ -10,7 +10,7 @@ import org.aeonbits.owner.Config.Sources;
 })
 public interface Internal extends EngineProperties {
     @Key("shaftEngineVersion")
-    @DefaultValue("7.3.20230824")
+    @DefaultValue("7.3.20230825")
     String shaftEngineVersion();
 
     @Key("watermarkImagePath")
