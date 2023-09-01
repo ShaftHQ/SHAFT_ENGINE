af89279dd31829c4f4e8cbb7f9d99f57aabe4b0e
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2023-09-01 19:36:20 +0300

    1268 feature request extract dsl into an independent project shaft dsl (#1272)
    * removing DSL package and Tests
    
    Signed-off-by: Mohab Mohie <Mohab.MohieElDeen@outlook.com>
    
    * version bump for release
    
    Signed-off-by: Mohab Mohie <Mohab.MohieElDeen@outlook.com>
    
    ---------
    
    Signed-off-by: Mohab Mohie <Mohab.MohieElDeen@outlook.com>

--- src/main/java/com/shaft/dsl/gui/CheckBox.java
+++
@@ -1,9 +0,0 @@
-package com.shaft.dsl.gui;
-
-import org.openqa.selenium.By;
-
-public class CheckBox extends RadioButton {
-    public CheckBox(By buttonLocator, By statusLocator) {
-        super(buttonLocator, statusLocator);
-    }
-}
