2c8c79538be69f50ec4ada75d16df40c6fe968d1
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2024-01-15 21:36:16 +0200

    Update Internal.java
    Signed-off-by: Mohab Mohie <Mohab.MohieElDeen@outlook.com>

--- src/main/java/com/shaft/properties/internal/Internal.java
+++ src/main/java/com/shaft/properties/internal/Internal.java
@@ -10,7 +10,7 @@ import org.aeonbits.owner.Config.Sources;
 })
 public interface Internal extends EngineProperties {
     @Key("shaftEngineVersion")
-    @DefaultValue("8.1.20240102")
+    @DefaultValue("8.1.20240115")
     String shaftEngineVersion();
 
     @Key("watermarkImagePath")
