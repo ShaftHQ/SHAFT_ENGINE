5dbe73572c1abc6b16266f47ca383cfbce45a3f3
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2024-03-24 01:19:06 +0200

    Merge pull request #1551
    Release 8.1.20240324

--- src/main/java/com/shaft/properties/internal/Internal.java
+++ src/main/java/com/shaft/properties/internal/Internal.java
@@ -10,7 +10,7 @@ import org.aeonbits.owner.Config.Sources;
 })
 public interface Internal extends EngineProperties {
     @Key("shaftEngineVersion")
-    @DefaultValue("8.1.20240224")
+    @DefaultValue("8.1.20240324")
     String shaftEngineVersion();
 
     @Key("watermarkImagePath")
