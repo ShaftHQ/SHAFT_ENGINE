12609136f1d4a358479f9b9d55e98731ce9a46c5
MustafaAgamy <mostafaagamy1995@gmail.com>
2024-04-07 16:08:49 +0300

    Add more strategies to FlutterFindingStrategy.java

--- src/main/java/com/shaft/enums/internal/FlutterFindingStrategy.java
+++ src/main/java/com/shaft/enums/internal/FlutterFindingStrategy.java
@@ -1,5 +1,5 @@
 package com.shaft.enums.internal;
 
 public enum FlutterFindingStrategy {
-    VALUE_KEY_STRING, VALUE_KEY_INT, TYPE, TEXT
+    VALUE_KEY_STRING, VALUE_KEY_INT, TYPE, TEXT, DESCENDANT, ANCESTOR
 }
