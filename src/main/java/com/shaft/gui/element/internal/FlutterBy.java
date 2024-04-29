63379eafcec0f5198d07cbf3585db91931569c00
MustafaAgamy <mostafaagamy1995@gmail.com>
2024-04-29 12:58:41 +0300

    Added Documenation
    Added Code Documentation to the FlutterBy Class

--- src/main/java/com/shaft/gui/element/internal/FlutterBy.java
+++ src/main/java/com/shaft/gui/element/internal/FlutterBy.java
@@ -7,6 +7,7 @@ import com.shaft.tools.internal.support.JavaHelper;
 import com.shaft.tools.io.internal.ReportManagerHelper;
 import io.github.ashwith.flutter.FlutterElement;
 import io.github.ashwith.flutter.FlutterFinder;
+import lombok.Getter;
 import org.openqa.selenium.By;
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebElement;
@@ -16,43 +17,98 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
-//ToDo -> make sure elementInformation object is fully utilized for flutter elements fast identification
 @SuppressWarnings("unused")
 @Beta
 public abstract class FlutterBy extends By implements By.Remotable {
     private final FlutterFindingStrategy flutterFindingStrategy;
+    @Getter
     private final By.Remotable.Parameters remoteParameters;
 
+    /**
+     * Constructs a new {@code FlutterBy} instance with the specified finding strategy and locator string.
+     *
+     * @param findingStrategy The finding strategy for locating Flutter elements.
+     * @param locatorString   The locator string used for identifying elements.
+     * @throws IllegalArgumentException If the locator string is null or empty.
+     */
     protected FlutterBy(FlutterFindingStrategy findingStrategy, String locatorString) {
         Preconditions.checkArgument(!Strings.isNullOrEmpty(locatorString), "Must supply a not empty locator value.");
         this.remoteParameters = new By.Remotable.Parameters(findingStrategy.name(), locatorString);
         this.flutterFindingStrategy = findingStrategy;
     }
 
+    /**
+     * Locates elements by their visible text.
+     *
+     * @param text The visible text of the element to locate.
+     * @return A {@code FlutterBy} instance representing the strategy for locating elements by text.
+     */
     public static FlutterBy text(String text) {
         return new ByText(text);
     }
 
+    /**
+     * Locates elements by their value key (String).
+     *
+     * @param valueKey The value key (String) of the element to locate.
+     * @return A {@code FlutterBy} instance representing the strategy for locating elements by value key (String).
+     */
     public static FlutterBy valueKey(String valueKey) {
         return new ByValueKeyString(valueKey);
     }
 
+    /**
+     * Locates elements by their value key (int).
+     *
+     * @param valueKey The value key (int) of the element to locate.
+     * @return A {FlutterBy} instance representing the strategy for locating elements by value key (int).
+     */
     public static FlutterBy valueKey(int valueKey) {
         return new ByValueKeyInt(valueKey);
     }
 
+    /**
+     * Locates elements by their type.
+     *
+     * @param type The type of the element to locate.
+     * @return A {@code FlutterBy} instance representing the strategy for locating elements by type.
+     */
     public static FlutterBy type(String type) {
         return new ByType(type);
     }
 
+    /**
+     * Locates descendant elements.
+     *
+     * @param of             The parent element.
+     * @param matching       The child element to match.
+     * @param matchRoot      Whether to match the root element.
+     * @param firstMatchOnly Whether to match only the first occurrence.
+     * @return A {@code FlutterBy} instance representing the strategy for locating descendant elements.
+     */
     public static FlutterBy descendant(FlutterBy of, FlutterBy matching, boolean matchRoot, boolean firstMatchOnly) {
         return new ByDescendant(of, matching, matchRoot, firstMatchOnly);
     }
 
+    /**
+     * Locates ancestor elements.
+     *
+     * @param of             The child element.
+     * @param matching       The ancestor element to match.
+     * @param matchRoot      Whether to match the root element.
+     * @param firstMatchOnly Whether to match only the first occurrence.
+     * @return A {@code FlutterBy} instance representing the strategy for locating ancestor elements.
+     */
     public static FlutterBy ancestor(FlutterBy of, FlutterBy matching, boolean matchRoot, boolean firstMatchOnly) {
         return new ByAncestor(of, matching, matchRoot, firstMatchOnly);
     }
 
+    /**
+     * Identifies the Flutter element based on the specified finder and the finding strategy.
+     *
+     * @param finder The Flutter finder to use for identifying elements.
+     * @return A list of objects representing the identified Flutter element.
+     */
     public List<Object> identifyFlutterElement(FlutterFinder finder) {
         try {
             switch (flutterFindingStrategy) {
@@ -82,6 +138,14 @@ public abstract class FlutterBy extends By implements By.Remotable {
             ReportManagerHelper.logDiscrete(e);
         }
         return Collections.emptyList();
+    }
+
+    private static ElementInformation createElementInformation(FlutterElement element, FlutterBy locator) {
+        ElementInformation elementInformation = new ElementInformation();
+        elementInformation.setFirstElement(element);
+        elementInformation.setLocator(locator);
+        elementInformation.setElementName(JavaHelper.formatLocatorToString(locator));
+        return elementInformation;
     }
 
     @Override
@@ -120,14 +184,16 @@ public abstract class FlutterBy extends By implements By.Remotable {
         return Objects.hash(super.hashCode(), this.remoteParameters.value(), this.flutterFindingStrategy.name());
     }
 
-    public Parameters getRemoteParameters() {
-        return this.remoteParameters;
-    }
-
+    /**
+     * Enumeration of Flutter finding strategies.
+     */
     public enum FlutterFindingStrategy {
         VALUE_KEY_STRING, VALUE_KEY_INT, TYPE, TEXT, DESCENDANT, ANCESTOR
     }
 
+    /**
+     * The Class for locating elements by their visible text.
+     */
     public static class ByText extends FlutterBy {
         private final String text;
 
@@ -138,14 +204,14 @@ public abstract class FlutterBy extends By implements By.Remotable {
         }
 
         private List<Object> identifyElement(FlutterFinder finder, String text) {
-            ElementInformation elementInformation = new ElementInformation();
-            elementInformation.setFirstElement(finder.byText(text));
-            elementInformation.setLocator(this);
-            elementInformation.setElementName(JavaHelper.formatLocatorToString(this));
+            ElementInformation elementInformation = createElementInformation(finder.byText(text), this);
             return elementInformation.toList();
         }
     }
 
+    /**
+     * The Class for locating elements by their value key (String).
+     */
     public static class ByValueKeyString extends FlutterBy implements Serializable {
         private final String valueKey;
 
@@ -163,6 +229,9 @@ public abstract class FlutterBy extends By implements By.Remotable {
         }
     }
 
+    /**
+     * The Class for locating elements by their value key (int).
+     */
     public static class ByValueKeyInt extends FlutterBy implements Serializable {
         private final int valueKey;
 
@@ -180,6 +249,9 @@ public abstract class FlutterBy extends By implements By.Remotable {
         }
     }
 
+    /**
+     * The Class for locating elements by their type.
+     */
     public static class ByType extends FlutterBy implements Serializable {
         private final String type;
 
@@ -197,6 +269,9 @@ public abstract class FlutterBy extends By implements By.Remotable {
         }
     }
 
+    /**
+     * The Class for locating elements by their descendant.
+     */
     public static class ByDescendant extends FlutterBy implements Serializable {
         private final FlutterBy of;
         private final FlutterBy matching;
@@ -221,6 +296,9 @@ public abstract class FlutterBy extends By implements By.Remotable {
         }
     }
 
+    /**
+     * The Class for locating elements by their ancestor.
+     */
     public static class ByAncestor extends FlutterBy implements Serializable {
         private final FlutterBy of;
         private final FlutterBy matching;
