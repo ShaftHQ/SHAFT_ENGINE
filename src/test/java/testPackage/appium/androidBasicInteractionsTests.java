5bab34ca41b654d047b2b38cd714f6772e6eb418
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2024-07-23 22:15:50 +0300

    w3c compliant mobile: scripts

--- src/test/java/testPackage/appium/androidBasicInteractionsTests.java
+++ src/test/java/testPackage/appium/androidBasicInteractionsTests.java
@@ -124,7 +124,7 @@ public class androidBasicInteractionsTests {
     public void testSendKeys() {
         String SEARCH_ACTIVITY = ".app.SearchInvoke";
 
-        ((AndroidDriver) driver.get().getDriver()).executeScript("startActivity", ImmutableMap.of("intent", PACKAGE + "/" + SEARCH_ACTIVITY));
+        ((AndroidDriver) driver.get().getDriver()).executeScript("mobile:startActivity", ImmutableMap.of("intent", PACKAGE + "/" + SEARCH_ACTIVITY));
 //        ((AndroidDriver) driver.get().getDriver()).startActivity(new Activity(PACKAGE, SEARCH_ACTIVITY));
 
         driver.get().element().type(By.id("txt_query_prefill"), "Hello world!")
@@ -137,7 +137,7 @@ public class androidBasicInteractionsTests {
         // Open the "Alert Dialog" activity of the android app
         String ALERT_DIALOG_ACTIVITY = ".app.AlertDialogSamples";
 
-        ((AndroidDriver) driver.get().getDriver()).executeScript("startActivity", ImmutableMap.of("intent", PACKAGE + "/" + ALERT_DIALOG_ACTIVITY));
+        ((AndroidDriver) driver.get().getDriver()).executeScript("mobile:startActivity", ImmutableMap.of("intent", PACKAGE + "/" + ALERT_DIALOG_ACTIVITY));
 //        ((AndroidDriver) driver.get().getDriver()).startActivity(new Activity(PACKAGE, ALERT_DIALOG_ACTIVITY));
 
         // Click button that opens a dialog
