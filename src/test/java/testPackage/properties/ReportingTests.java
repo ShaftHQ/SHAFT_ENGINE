f807a8decd16d8a9d55a5865ca60408529997f8f
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2024-08-13 11:22:38 +0300

    revamp allure report properties
    - extract Allure properties to a separate file
    - remove traces of Allure properties from Reporting.java
    - clean allure report directory automatically and keep the file name "index.html" when the "accumulateReports" property is disabled (enabled by default)
    - automaticallyOpen (used to be: openAllureReportAfterExecution)
    - accumulateHistory (used to be: cleanAllureResultsDirectoryBeforeExecution)
    - generateArchive (used to be: generateAllureReportArchive)
    - introduced "customLogo" property to override the default report logo
    - introduced "customTitle" property to override the default report title
    - closes #1566
    - closes #1672

--- src/test/java/testPackage/properties/ReportingTests.java
+++ src/test/java/testPackage/properties/ReportingTests.java
@@ -9,9 +9,6 @@ public class ReportingTests {
     boolean captureWebDriverLogs;
     boolean alwaysLogDiscreetly;
     boolean debugMode;
-    boolean cleanAllureResultsDirectoryBeforeExecution;
-    boolean generateAllureReportArchive;
-    boolean openAllureReportAfterExecution;
     boolean openLighthouseReportWhileExecution;
     boolean openExecutionSummaryReportAfterExecution;
     boolean disableLogging;
@@ -22,9 +19,6 @@ public class ReportingTests {
         captureWebDriverLogs = SHAFT.Properties.reporting.captureWebDriverLogs();
         alwaysLogDiscreetly = SHAFT.Properties.reporting.alwaysLogDiscreetly();
         debugMode = SHAFT.Properties.reporting.debugMode();
-        cleanAllureResultsDirectoryBeforeExecution = SHAFT.Properties.reporting.cleanAllureResultsDirectoryBeforeExecution();
-        generateAllureReportArchive = SHAFT.Properties.reporting.generateAllureReportArchive();
-        openAllureReportAfterExecution = SHAFT.Properties.reporting.openAllureReportAfterExecution();
         openLighthouseReportWhileExecution = SHAFT.Properties.reporting.openLighthouseReportWhileExecution();
         openExecutionSummaryReportAfterExecution = SHAFT.Properties.reporting.openExecutionSummaryReportAfterExecution();
         disableLogging = SHAFT.Properties.reporting.disableLogging();
@@ -37,11 +31,8 @@ public class ReportingTests {
         SHAFT.Properties.reporting.set().captureWebDriverLogs(captureWebDriverLogs);
         SHAFT.Properties.reporting.set().alwaysLogDiscreetly(alwaysLogDiscreetly);
         SHAFT.Properties.reporting.set().debugMode(debugMode);
-        SHAFT.Properties.reporting.set().generateAllureReportArchive(generateAllureReportArchive);
-        SHAFT.Properties.reporting.set().openAllureReportAfterExecution(openAllureReportAfterExecution);
         SHAFT.Properties.reporting.set().openLighthouseReportWhileExecution(openLighthouseReportWhileExecution);
         SHAFT.Properties.reporting.set().openExecutionSummaryReportAfterExecution(openExecutionSummaryReportAfterExecution);
         SHAFT.Properties.reporting.set().disableLogging(disableLogging);
-
     }
 }
