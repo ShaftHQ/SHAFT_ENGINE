db23a33f8518733a0629774bbb9d98a3d84ae104
Ahmed Zanoon <65794900+zanoon2020@users.noreply.github.com>
2024-08-24 20:36:49 +0300

    enhance allure properties readability (#1711)

--- src/main/java/com/shaft/properties/internal/Allure.java
+++ src/main/java/com/shaft/properties/internal/Allure.java
@@ -17,27 +17,27 @@ public interface Allure extends EngineProperties<Allure> {
             ReportManager.logDiscrete("Setting \"" + key + "\" property with \"" + value + "\".");
     }
 
-    @Key("automaticallyOpen")
+    @Key("allure.automaticallyOpen")
     @DefaultValue("true")
     boolean automaticallyOpen(); //automaticallyOpen (used to be: openAllureReportAfterExecution)
 
-    @Key("accumulateHistory")
+    @Key("allure.accumulateHistory")
     @DefaultValue("false")
     boolean accumulateHistory(); //accumulateHistory (used to be: cleanAllureResultsDirectoryBeforeExecution)
 
-    @Key("accumulateReports")
+    @Key("allure.accumulateReports")
     @DefaultValue("true")
     boolean accumulateReports(); //allows html files to accumulate in the allure report directory
 
-    @Key("generateArchive")
+    @Key("allure.generateArchive")
     @DefaultValue("false")
     boolean generateArchive(); //generateArchive (used to be: generateAllureReportArchive)
 
-    @Key("customLogo")
+    @Key("allure.customLogo")
     @DefaultValue("https://github.com/ShaftHQ/SHAFT_ENGINE/blob/main/src/main/resources/images/shaft_white.png?raw=true")
     String customLogo();
 
-    @Key("customTitle")
+    @Key("allure.customTitle")
     @DefaultValue("Test run report")
     String customTitle();
 
@@ -48,35 +48,32 @@ public interface Allure extends EngineProperties<Allure> {
     class SetProperty implements EngineProperties.SetProperty {
 
         public SetProperty automaticallyOpen(boolean value) {
-            setProperty("automaticallyOpen", String.valueOf(value));
-            setProperty("openAllureReportAfterExecution", String.valueOf(value));
+            setProperty("allure.automaticallyOpen", String.valueOf(value));
             return this;
         }
 
         public SetProperty accumulateHistory(boolean value) {
-            setProperty("accumulateHistory", String.valueOf(value));
-            setProperty("cleanAllureResultsDirectoryBeforeExecution", String.valueOf(value));
+            setProperty("allure.accumulateHistory", String.valueOf(value));
             return this;
         }
 
         public SetProperty accumulateReports(boolean value) {
-            setProperty("accumulateReports", String.valueOf(value));
+            setProperty("allure.accumulateReports", String.valueOf(value));
             return this;
         }
 
         public SetProperty generateArchive(boolean value) {
-            setProperty("generateArchive", String.valueOf(value));
-            setProperty("generateAllureReportArchive", String.valueOf(value));
+            setProperty("allure.generateArchive", String.valueOf(value));
             return this;
         }
 
         public SetProperty customLogo(String value) {
-            setProperty("customLogo", value);
+            setProperty("allure.customLogo", value);
             return this;
         }
 
         public SetProperty customTitle(String value) {
-            setProperty("customTitle", value);
+            setProperty("allure.customTitle", value);
             return this;
         }
 
