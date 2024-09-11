45e9d60830a5c39ec25ce462e016f1103e989028
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2024-09-11 22:41:43 +0300

    reselect after change

--- src/test/java/testPackage/mockedTests/DbTests.java
+++ src/test/java/testPackage/mockedTests/DbTests.java
@@ -21,18 +21,21 @@ public class DbTests {
     @Test(dependsOnMethods = {"select"})
     public void insert() {
         databaseInstance.executeInsertQuery("INSERT INTO student(firstname, lastname, age, address, email) VALUES('Ahmed the', 'Octocat', 9, '88 Colin P Kelly Jr St, San Francisco, CA 94107, United States', 'ahmedoctocat@github.com')");
+        databaseInstance.executeSelectQuery("SELECT * FROM student");
         SHAFT.Validations.assertThat().object(databaseInstance.getResult()).contains("Ahmed");
     }
 
     @Test(dependsOnMethods = {"select","insert"})
     public void update() {
         databaseInstance.executeUpdateQuery("UPDATE student SET age = 8 WHERE email = octocat@github.com';");
+        databaseInstance.executeSelectQuery("SELECT * FROM student");
         SHAFT.Validations.assertThat().object(databaseInstance.getResult()).contains("8");
     }
 
     @Test(dependsOnMethods = {"select","insert","update"})
     public void delete() {
         databaseInstance.executeDeleteQuery("DELETE FROM student WHERE email = octocat@github.com';");
+        databaseInstance.executeSelectQuery("SELECT * FROM student");
         SHAFT.Validations.assertThat().object(databaseInstance.getResult()).doesNotContain("8");
     }
 
