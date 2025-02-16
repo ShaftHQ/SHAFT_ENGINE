f3aa937a84ae3ca034f3ebe5014e6fe889f54c11
Mohab Mohie <mohab.mohieeldeen@outlook.com>
2025-02-16 14:02:49 +0200

    finally caught it

--- src/main/java/com/shaft/properties/internal/PropertiesHelper.java
+++ src/main/java/com/shaft/properties/internal/PropertiesHelper.java
@@ -184,7 +184,7 @@ public class PropertiesHelper {
 
         // override target properties only if they do not exist
         var finalPropertiesFolderPath = propertiesFolderPath;
-        Arrays.asList("/cucumber.properties", "/customWebdriverCapabilities.properties", "/log4j2.properties", "/TestNG.properties", "/reportPortal.properties")
+        Arrays.asList("/cucumber.properties", "/customWebdriverCapabilities.properties", "/log4j2.properties", "/TestNG.properties", "/reportportal.properties")
                 .forEach(file -> {
                     if (!fileActions.doesFileExist(TARGET_PROPERTIES_FOLDER_PATH + file)) {
                         if (isExternalRun) {
