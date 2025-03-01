8ae350498430d90a5324c5286163eb28c7d6362a
Kyrillos Nageh Abdelnour Flamon <kyrillosnageh@3ddx.com>
2025-03-01 13:06:14 +0200

    Refactored API Request Parameters to use Map<String, Object> for improved usability

--- src/test/java/testPackage/legacy/BasicAPITests.java
+++ src/test/java/testPackage/legacy/BasicAPITests.java
@@ -54,4 +54,16 @@ public class BasicAPITests {
                 .perform();
 
     }
+
+    @Test
+    public void apiTest4() {
+        Map <String, Object> formParameters = Map.of("name", "SHAFT",
+                                                     "job", "Automation Engineer");
+        api = new SHAFT.API("https://reqres.in/api");
+
+        api.post("users")
+                .setParameters(formParameters, RestActions.ParametersType.FORM)
+                .setTargetStatusCode(200)
+                .perform();
+    }
 }
