b9a9783377b2faa8573e37b8e1b408f6c86ac9db
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2025-07-12 18:42:11 +0300

    changing default values - readinessState set to 'interactive' rather than 'none' - pageLoadStrategy set to 'eager' rather than 'none'

--- src/main/java/com/shaft/properties/internal/Web.java
+++ src/main/java/com/shaft/properties/internal/Web.java
@@ -74,12 +74,12 @@ public interface Web extends EngineProperties<Web> {
 
     // none, eager, or normal
     @Key("pageLoadStrategy")
-    @DefaultValue("normal")
+    @DefaultValue("eager")
     String pageLoadStrategy();
 
     // interactive, none, or complete
     @Key("readinessState")
-    @DefaultValue("complete")
+    @DefaultValue("interactive")
     String readinessState();
 
     default SetProperty set() {
