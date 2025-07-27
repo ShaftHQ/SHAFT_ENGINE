9393285899dd46a18246b4b50bcba097498d9d3b
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2025-07-27 13:06:39 +0300

    tweaks (#2048)
    * add custom properties to generated files
    
    * add support for parent and child
    
    * adding byRole, renaming byRelation and byAxis
    
    * adding inputField and clickableField
    
    * adding @NotNull

--- src/main/java/com/shaft/gui/internal/locator/Locator.java
+++ src/main/java/com/shaft/gui/internal/locator/Locator.java
@@ -1,12 +1,31 @@
 package com.shaft.gui.internal.locator;
 
+import lombok.NonNull;
+import org.openqa.selenium.Beta;
+import org.openqa.selenium.By;
+
 public class Locator {
 
-    public static LocatorBuilder hasTagName(String tagName) {
+    public static LocatorBuilder hasTagName(@NonNull String tagName) {
         return LocatorBuilder.hasTagName(tagName);
     }
 
     public static LocatorBuilder hasAnyTagName() {
         return LocatorBuilder.hasTagName("*");
+    }
+
+    @Beta
+    public static LocatorBuilder hasRole(@NonNull Role ariaRole) {
+        return LocatorBuilder.byRole(ariaRole);
+    }
+
+    @Beta
+    public static By inputField(@NonNull String elementName) {
+        return SmartLocators.inputField(elementName);
+    }
+
+    @Beta
+    public static By clickableField(@NonNull String elementName) {
+        return SmartLocators.clickableField(elementName);
     }
 }
