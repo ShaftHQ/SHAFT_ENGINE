5976990380d1cf46e8e20bc29bff4e0db1ddbd6f
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2025-08-16 08:39:52 +0300

    enhance scrolling logic + version bump

--- src/main/java/com/shaft/properties/internal/Internal.java
+++ src/main/java/com/shaft/properties/internal/Internal.java
@@ -10,7 +10,7 @@ import org.aeonbits.owner.Config.Sources;
 })
 public interface Internal extends EngineProperties<Internal> {
     @Key("shaftEngineVersion")
-    @DefaultValue("9.3.20250814")
+    @DefaultValue("9.3.20250816")
     String shaftEngineVersion();
 
     @Key("watermarkImagePath")
