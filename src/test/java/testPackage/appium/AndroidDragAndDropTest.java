57c4bd00ea0943998d13e1eac7bc56aa98ecd9e1
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2025-09-10 09:29:44 +0300

    fix android tests setup

--- src/test/java/testPackage/appium/AndroidDragAndDropTest.java
+++ src/test/java/testPackage/appium/AndroidDragAndDropTest.java
@@ -7,24 +7,7 @@ import org.testng.annotations.Test;
 public class AndroidDragAndDropTest extends MobileTest {
     private static final By dragAndDropScreen = AppiumBy.accessibilityId("Drag-drop-screen");
 
-    @Test
-    public void wizard_scrollInExpandableLists_verticalScrolling_insideScreen() {
-        By dragButton = By.xpath("//android.widget.Button[@content-desc='Drag']");
-        By draggableRobotEyes = By.xpath("//android.view.ViewGroup[@content-desc='drag-c1']/android.widget.ImageView");
-        By dropRobotEyes = By.xpath("//android.view.ViewGroup[@content-desc='drop-c1']/android.view.ViewGroup");
-
-        // WebDriver code -> really fails to drag
-//        driver.get().element().click(dragButton)
-//                .dragAndDrop(draggableRobotEyes, dropRobotEyes)
-//                .and().assertThat(dragAndDropScreen).matchesReferenceImage().perform();
-
-        // Appium code -> drag and drop happens but shows up as failed
-        driver.get().touch().tap(dragButton)
-                .swipeToElement(draggableRobotEyes, dropRobotEyes)
-                .and().assertThat(dragAndDropScreen).matchesReferenceImage().perform();
-    }
-
-    @Test
+    @Test(groups = {"NativeAndroidDemo"})
     public void multipleDragAndDrop() {
         driver.get().touch().tap(AppiumBy.accessibilityId("Drag"));
         driver.get().element()
--- src/test/java/testPackage/appium/MobileTest.java
+++ src/test/java/testPackage/appium/MobileTest.java
@@ -9,9 +9,47 @@ import org.testng.annotations.BeforeMethod;
 public abstract class MobileTest {
     public static final ThreadLocal<SHAFT.GUI.WebDriver> driver = new ThreadLocal<>();
 
-    @SuppressWarnings("CommentedOutCode")
-    @BeforeMethod
-    public void setup() {
+    @BeforeMethod(onlyForGroups = {"NativeAndroidDemo"})
+    public void setupNativeAndroidDemoApk() {
+        System.setProperty("mobile_autoGrantPermissions", "true");
+
+        SHAFT.Properties.flags.set().forceCheckElementLocatorIsUnique(false);
+        // common attributes
+        SHAFT.Properties.platform.set().targetPlatform(Platform.ANDROID.name());
+        SHAFT.Properties.mobile.set().automationName(AutomationName.ANDROID_UIAUTOMATOR2);
+
+        // self-managed execution
+//        SHAFT.Properties.mobile.set().selfManaged(true);
+//        SHAFT.Properties.mobile.set().selfManagedAndroidSDKVersion(31);
+
+        // local appium server (for local and GitHub actions execution)
+//        SHAFT.Properties.platform.set().executionAddress("localhost:4723");
+//        SHAFT.Properties.mobile.set().app("src/test/resources/testDataFiles/apps/Android-NativeDemoApp-0.4.0.apk");
+
+        // local appium server (android-emulator docker-compose)
+//        SHAFT.Properties.platform.set().executionAddress("localhost:4725");
+//        SHAFT.Properties.mobile.set().app("src/test/resources/testDataFiles/apps/Android-NativeDemoApp-0.4.0.apk");
+
+        // remote browserstack server (new app version)
+        SHAFT.Properties.platform.set().executionAddress("browserstack");
+        SHAFT.Properties.browserStack.set().platformVersion("13.0");
+        SHAFT.Properties.browserStack.set().deviceName("Google Pixel 7");
+        SHAFT.Properties.browserStack.set().appName("Android-NativeDemoApp.apk");
+        SHAFT.Properties.browserStack.set().appRelativeFilePath("src/test/resources/testDataFiles/apps/Android-NativeDemoApp-0.4.0.apk");
+        SHAFT.Properties.browserStack.set().appUrl("");
+
+        // remote browserstack server (existing app version)
+//        SHAFT.Properties.platform.set().executionAddress("browserstack");
+//        SHAFT.Properties.browserStack.set().platformVersion("13.0");
+//        SHAFT.Properties.browserStack.set().deviceName("Google Pixel 7");
+//        SHAFT.Properties.browserStack.set().appName("Android-NativeDemoApp.apk");
+//        SHAFT.Properties.browserStack.set().appRelativeFilePath("");
+//        SHAFT.Properties.browserStack.set().appUrl("bs://e744ef24a081b0d4cb5f9699a5dd69d6a3a2dbce");
+        driver.set(new SHAFT.GUI.WebDriver());
+    }
+
+    @BeforeMethod(onlyForGroups = {"ApiDemosDebug"})
+    public void setupApiDemosDebug() {
         System.setProperty("mobile_autoGrantPermissions", "true");
 
         SHAFT.Properties.flags.set().forceCheckElementLocatorIsUnique(false);
@@ -32,12 +70,12 @@ public abstract class MobileTest {
 //        SHAFT.Properties.mobile.set().app("src/test/resources/testDataFiles/apps/ApiDemos-debug.apk");
 
         // remote browserstack server (new app version)
-//        SHAFT.Properties.platform.set().executionAddress("browserstack");
-//        SHAFT.Properties.browserStack.set().platformVersion("13.0");
-//        SHAFT.Properties.browserStack.set().deviceName("Google Pixel 7");
-//        SHAFT.Properties.browserStack.set().appName("ApiDemos-debug.apk");
-//        SHAFT.Properties.browserStack.set().appRelativeFilePath("src/test/resources/testDataFiles/apps/ApiDemos-debug.apk");
-//        SHAFT.Properties.browserStack.set().appUrl("");
+        SHAFT.Properties.platform.set().executionAddress("browserstack");
+        SHAFT.Properties.browserStack.set().platformVersion("13.0");
+        SHAFT.Properties.browserStack.set().deviceName("Google Pixel 7");
+        SHAFT.Properties.browserStack.set().appName("ApiDemos-debug.apk");
+        SHAFT.Properties.browserStack.set().appRelativeFilePath("src/test/resources/testDataFiles/apps/ApiDemos-debug.apk");
+        SHAFT.Properties.browserStack.set().appUrl("");
 
         // remote browserstack server (existing app version)
 //        SHAFT.Properties.platform.set().executionAddress("browserstack");
