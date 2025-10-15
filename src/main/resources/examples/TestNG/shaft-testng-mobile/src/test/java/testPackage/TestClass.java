dceddd5ae644792dec33cd96d5fd3e2a45528d2b
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2025-10-15 10:07:39 +0300

    shaft-testng-mobile

---
+++ src/main/resources/examples/TestNG/shaft-testng-mobile/src/test/java/testPackage/TestClass.java
@@ -0,0 +1,51 @@
+package testPackage;
+
+import com.shaft.driver.SHAFT;
+import com.shaft.gui.element.TouchActions;
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class TestClass {
+    SHAFT.GUI.WebDriver driver;
+    SHAFT.TestData.JSON testData;
+
+    @Test
+    public void navigateToDuckDuckGoAndAssertBrowserTitleIsDisplayedCorrectly() {
+        ((AndroidDriver) driver.getDriver()).runAppInBackground(Duration.ofSeconds(5));
+        driver.element().performTouchAction()
+                .swipeElementIntoView(AppiumBy.accessibilityId("Views"), TouchActions.SwipeDirection.DOWN)
+                .tap(AppiumBy.accessibilityId("Views"))
+                .swipeElementIntoView(AppiumBy.accessibilityId("Expandable Lists"), TouchActions.SwipeDirection.DOWN)
+                .tap(AppiumBy.accessibilityId("Expandable Lists"))
+                .swipeElementIntoView(AppiumBy.accessibilityId("3. Simple Adapter"), TouchActions.SwipeDirection.DOWN)
+                .tap(AppiumBy.accessibilityId("3. Simple Adapter"))
+                .swipeElementIntoView(By.xpath("//android.widget.TextView[@text='Group 18']"), TouchActions.SwipeDirection.DOWN)
+                .tap(By.xpath("//android.widget.TextView[@text='Group 18']"))
+                .swipeElementIntoView(By.xpath("//android.widget.TextView[@text='Child 13']"), TouchActions.SwipeDirection.DOWN)
+                .swipeElementIntoView(By.xpath("//android.widget.TextView[@text='Group 1']"), TouchActions.SwipeDirection.UP)
+                .sendAppToBackground(1)
+                .assertThat(By.xpath("//android.widget.TextView[@text='Group 1']")).exists();
+    }
+
+    @BeforeClass
+    public void beforeClass() {
+        testData = new SHAFT.TestData.JSON("simpleJSON.json");
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        driver = new SHAFT.GUI.WebDriver();
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        driver.quit();
+    }
+}
