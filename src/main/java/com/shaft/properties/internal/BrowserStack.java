6d4c046da6a6be81331f0c8af471b0147aaf90f1
MustafaAgamy <mostafaagamy1995@gmail.com>
2026-01-03 11:56:15 +0200

    Fix browser stack tests (#2234)
    * - Fix BrowserStack CI Tests:
       - Added New Default userName and accesskey values at BrowserStack.java under properties.internal for further identifying of the issue whether it's related to authentication attempt failure or not
    
    * - Fix BrowserStack CI Tests:
       - Added New Default userName and accesskey values at BrowserStack.java under properties.internal as a new default values

--- src/main/java/com/shaft/properties/internal/BrowserStack.java
+++ src/main/java/com/shaft/properties/internal/BrowserStack.java
@@ -30,11 +30,11 @@ public interface BrowserStack extends EngineProperties<BrowserStack> {
 
     //Below properties are all required
     @Key("browserStack.userName")
-    @DefaultValue("mohabmohie1")
+    @DefaultValue("mostafaagamy_aNA9cE")
     String userName();
 
     @Key("browserStack.accessKey")
-    @DefaultValue("")
+    @DefaultValue("CBNU4Ke25x81G1yQtsRT")
     String accessKey();
 
     //Below properties are needed for native mobile app testing:
