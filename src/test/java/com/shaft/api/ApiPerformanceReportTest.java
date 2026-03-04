b466e8a7a6412fcce912976e7566856480272376
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2026-03-04 14:49:01 +0200

    Update ApiPerformanceReportTest.java
    Signed-off-by: Mohab Mohie <Mohab.MohieElDeen@outlook.com>

--- src/test/java/com/shaft/api/ApiPerformanceReportTest.java
+++ src/test/java/com/shaft/api/ApiPerformanceReportTest.java
@@ -5,6 +5,7 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -200,7 +201,7 @@ public class ApiPerformanceReportTest {
                 .perform();
     }
 
-    @BeforeMethod
+    @AfterMethod
     public void cleanup() {
         api = new ThreadLocal<>();
     }
