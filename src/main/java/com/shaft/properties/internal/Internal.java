2eaee04b748b8e7a6b8bb84597b5c07096ce0846
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2026-06-05 21:22:02 +0300

    codex: prepare release 10.2.20260605 (#2802)

--- src/main/java/com/shaft/properties/internal/Internal.java
+++ src/main/java/com/shaft/properties/internal/Internal.java
@@ -18,7 +18,7 @@ import org.aeonbits.owner.Config.Sources;
 })
 public interface Internal extends EngineProperties<Internal> {
     @Key("shaftEngineVersion")
-    @DefaultValue("10.2.20260530")
+    @DefaultValue("10.2.20260605")
     String shaftEngineVersion();
 
     @Key("watermarkImagePath")
