dcce74aad5727e6ded149f2fe4df2268827b07e7
MohabMohie <MohabMohie@users.noreply.github.com>
2026-06-10 10:08:16 +0000

    Deploying to javadoc from @ ShaftHQ/SHAFT_ENGINE@705a19e36ecf735c2678370145cb9963450aa72e 🚀

---
+++ shaft-video/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"com.shaft.gui.internal.video","l":"AutomationRemarksDesktopVideoRecordingProvider"}];updateSearchResults();
