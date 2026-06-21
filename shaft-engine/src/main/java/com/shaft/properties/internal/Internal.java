8578e3973ef5e68434ba51807c6f620e10170c2b
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2026-06-21 07:03:33 +0300

    release 10.2.20260621

--- shaft-engine/src/main/java/com/shaft/properties/internal/Internal.java
+++ shaft-engine/src/main/java/com/shaft/properties/internal/Internal.java
@@ -18,7 +18,7 @@ import org.aeonbits.owner.Config.Sources;
 })
 public interface Internal extends EngineProperties<Internal> {
     @Key("shaftEngineVersion")
-    @DefaultValue("10.2.20260620")
+    @DefaultValue("10.2.20260621")
     String shaftEngineVersion();
 
     @Key("watermarkImagePath")
