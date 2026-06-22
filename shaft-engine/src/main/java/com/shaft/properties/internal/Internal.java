ea687efdccc1b01404f198748af960b1c695bc33
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2026-06-22 10:36:01 +0300

    Prepare SHAFT Engine release 10.2.20260622 (#3002)
    [codex] release 10.2.20260622

--- shaft-engine/src/main/java/com/shaft/properties/internal/Internal.java
+++ shaft-engine/src/main/java/com/shaft/properties/internal/Internal.java
@@ -18,7 +18,7 @@ import org.aeonbits.owner.Config.Sources;
 })
 public interface Internal extends EngineProperties<Internal> {
     @Key("shaftEngineVersion")
-    @DefaultValue("10.2.20260621")
+    @DefaultValue("10.2.20260622")
     String shaftEngineVersion();
 
     @Key("watermarkImagePath")
